a0431fac06a08f5f70ea1a681daa65a159ad9bc6
Mina Kang <kasong13@unist.ac.kr>
2021-02-09 00:52:46 +0900

    Trip plan Popup on implementing
    -showDialog
    -pop button implemented

--- prototype2021/lib/survey/survey_page.dart
+++ prototype2021/lib/survey/survey_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prototype2021/survey/templates/ticket_button.dart';
 import 'package:prototype2021/survey/templates/select_trip_plan.dart';
 import 'package:prototype2021/survey/templates/story_explanation.dart';
+import 'package:prototype2021/templates/borded_button.dart';
 
 class SurveyPage extends StatefulWidget {
   @override
@@ -13,53 +14,74 @@ class _SurveyPageState extends State<SurveyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          title: Text(
-            'Trip Builder',
-            style: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w900,
-              fontStyle: FontStyle.italic,
-              fontSize: 18,
-            ),
+        title: Text(
+          'Trip Builder',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w900,
+            fontStyle: FontStyle.italic,
+            fontSize: 18,
           ),
         ),
-        body: Column(
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
           children: [
-            StoryExplanation(
-              explanation: "TripBuilder Devloper",
-            ),
-            Column(
+            SelectTripPlan(),
+            SizedBox(height: 10),
+            Row(
               children: [
-                Row(
-                  children: [
-                    // FlatButton(
-                    //   child: Text("asdf"),
-                    //   onPressed: () {},
-                    // ),
-                    Toggle
-                  ],
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    child: BordedButton(
+                      child: Text("내 플랜 미리보기"),
+                      onPressed: openDialog,
+                      radius: 5,
+                    ),
+                  ),
                 ),
-                Row(
-                  children: [],
-                ),
-                Row(
-                  children: [],
-                )
               ],
             ),
-            // Expanded(
-            //   child: SingleChildScrollView(
-            //     child: Column(
-            //       children: [],
-            //     ),
-            //   ),
-            // ),
-            Container(height: 80, child: Text('asfd'))
           ],
-        ));
+        ),
+      ),
+    );
+  }
+
+  openDialog() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(32.0))),
+            contentPadding: EdgeInsets.only(top: 10.0),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "안녕안녕안녕안녕",
+                  ),
+                ),
+                Center(
+                  child: FlatButton(
+                    child: Text("닫기"),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
