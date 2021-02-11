c3aed24a408d81d6d1f21f5c8d488368ba8fdb44
Mina Kang <kasong13@unist.ac.kr>
2021-02-12 00:11:59 +0900

    [!Complete]SelectTripPlan Implemented
    -preview dialog implemented
    -fields containing value changed to integer list
    -dynamic preview implemented

--- prototype2021/lib/survey/survey_page.dart
+++ prototype2021/lib/survey/survey_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:prototype2021/survey/templates/ticket_button.dart';
 import 'package:prototype2021/survey/templates/select_trip_plan.dart';
 import 'package:prototype2021/survey/templates/story_explanation.dart';
-import 'package:prototype2021/templates/borded_button.dart';
 
 class SurveyPage extends StatefulWidget {
   @override
@@ -30,58 +29,8 @@ class _SurveyPageState extends State<SurveyPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            SelectTripPlan(),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 50,
-                    child: BordedButton(
-                      child: Text("내 플랜 미리보기"),
-                      onPressed: openDialog,
-                      radius: 5,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+        child: SelectTripPlan(),
       ),
     );
-  }
-
-  openDialog() {
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(32.0))),
-            contentPadding: EdgeInsets.only(top: 10.0),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    "안녕안녕안녕안녕",
-                  ),
-                ),
-                Center(
-                  child: FlatButton(
-                    child: Text("닫기"),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-              ],
-            ),
-          );
-        });
   }
 }
--- prototype2021/lib/survey/templates/select_trip_plan.dart
+++ prototype2021/lib/survey/templates/select_trip_plan.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype2021/templates/borded_button.dart';
 
 class SelectTripPlan extends StatefulWidget {
   @override
@@ -6,45 +7,214 @@ class SelectTripPlan extends StatefulWidget {
 }
 
 class _SelectTripPlanState extends State<SelectTripPlan> {
-  final _valueList = ['0', '1', '2', '3', '4', '5'];
-  List<String> _selectedValue = ['0', '0', '0', '0'];
+  final _valueList = [0, 1, 2, 3, 4, 5];
+  List<int> _selectedValue = [0, 0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          border: Border.all(color: Colors.greenAccent[100], width: 3),
-          color: Colors.greenAccent[100]),
-      height: 270,
-      child: Column(children: [
-        _buildSelectMenuBar(
-          0,
-          Icon(Icons.access_time_rounded, size: 30),
-          "아침 전",
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(color: Colors.greenAccent[100], width: 3),
+              color: Colors.greenAccent[100]),
+          height: 270,
+          child: Column(children: [
+            _buildSelectMenuBar(
+              0,
+              Icon(Icons.access_time_rounded, size: 30),
+              "아침 전",
+            ),
+            SizedBox(height: 8),
+            Container(height: 1, width: 320, color: Colors.white),
+            _buildSelectMenuBar(
+              1,
+              Icon(Icons.access_time_rounded, size: 30),
+              "아침과 점심 사이",
+            ),
+            SizedBox(height: 8),
+            Container(height: 1, width: 320, color: Colors.white),
+            _buildSelectMenuBar(
+              2,
+              Icon(Icons.access_time_rounded, size: 30),
+              "점심과 저녁 사이",
+            ),
+            SizedBox(height: 8),
+            Container(height: 1, width: 320, color: Colors.white),
+            _buildSelectMenuBar(
+              3,
+              Icon(Icons.access_time_rounded, size: 30),
+              "저녁 이후",
+            ),
+          ]),
         ),
-        SizedBox(height: 8),
-        Container(height: 1, width: 320, color: Colors.white),
-        _buildSelectMenuBar(
-          1,
-          Icon(Icons.access_time_rounded, size: 30),
-          "아침과 점심 사이",
+        SizedBox(height: 10),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 50,
+                child: BordedButton(
+                  child: Text("내 플랜 미리보기"),
+                  onPressed: openDialog,
+                  radius: 5,
+                ),
+              ),
+            ),
+          ],
         ),
-        SizedBox(height: 8),
-        Container(height: 1, width: 320, color: Colors.white),
-        _buildSelectMenuBar(
-          2,
-          Icon(Icons.access_time_rounded, size: 30),
-          "점심과 저녁 사이",
+      ],
+    );
+  }
+
+  openDialog() {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            insetPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
+              ),
+            ),
+            contentPadding: EdgeInsets.all(0),
+            content: Container(
+              height: double.maxFinite,
+              width: double.maxFinite,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                    padding: EdgeInsets.fromLTRB(15, 5, 15, 5),
+                    decoration: BoxDecoration(
+                      color: Colors.greenAccent[200],
+                      border: Border.all(color: Colors.greenAccent[200]),
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                    child: Text(
+                      "여행 플랜",
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 15),
+                      child: ListView.builder(
+                        itemCount: 3 +
+                            _selectedValue[0] +
+                            _selectedValue[1] +
+                            _selectedValue[2] +
+                            _selectedValue[3],
+                        itemBuilder: (BuildContext _context, int i) {
+                          if (i < _selectedValue[0]) {
+                            var idx = i + 1;
+                            return _buildPlanRow(
+                                i + 1, "여행지 $idx", Icons.location_on_outlined);
+                          } else if (i == _selectedValue[0]) {
+                            return _buildPlanRow(i + 1, "아침", Icons.restaurant);
+                          } else if (i <
+                              _selectedValue[0] + _selectedValue[1] + 1) {
+                            var idx = i;
+                            return _buildPlanRow(
+                                i + 1, "여행지 $idx", Icons.location_on_outlined);
+                          } else if (i ==
+                              _selectedValue[0] + _selectedValue[1] + 1) {
+                            return _buildPlanRow(i + 1, "점심", Icons.restaurant);
+                          } else if (i <
+                              _selectedValue[0] +
+                                  _selectedValue[1] +
+                                  _selectedValue[2] +
+                                  2) {
+                            var idx = i - 1;
+                            return _buildPlanRow(
+                                i + 1, "여행지 $idx", Icons.location_on_outlined);
+                          } else if (i ==
+                              _selectedValue[0] +
+                                  _selectedValue[1] +
+                                  _selectedValue[2] +
+                                  2) {
+                            return _buildPlanRow(i + 1, "저녁", Icons.restaurant);
+                          } else {
+                            var idx = i - 2;
+                            return _buildPlanRow(
+                                i + 1, "여행지 $idx", Icons.location_on_outlined);
+                          }
+                        },
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      width: double.maxFinite,
+                      height: 60,
+                      child: FlatButton(
+                        child: Text("닫기"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _buildPlanRow(int i, String text, IconData icon) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        (i == 1)
+            ? SizedBox()
+            : Container(
+                margin: EdgeInsets.only(left: 15),
+                height: 8,
+                width: 1,
+                color: Colors.black),
+        Row(
+          children: [
+            Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.grey)),
+              child: Center(
+                child: Text("$i"),
+              ),
+            ),
+            SizedBox(width: 5),
+            Icon(
+              icon,
+              size: 35,
+            ),
+            SizedBox(width: 5),
+            Text(text),
+          ],
         ),
-        SizedBox(height: 8),
-        Container(height: 1, width: 320, color: Colors.white),
-        _buildSelectMenuBar(
-          3,
-          Icon(Icons.access_time_rounded, size: 30),
-          "저녁 이후",
-        ),
-      ]),
+        (i ==
+                3 +
+                    _selectedValue[0] +
+                    _selectedValue[1] +
+                    _selectedValue[2] +
+                    _selectedValue[3])
+            ? SizedBox()
+            : Container(
+                margin: EdgeInsets.only(left: 15),
+                height: 8,
+                width: 1,
+                color: Colors.black),
+      ],
     );
   }
 
@@ -95,7 +265,7 @@ class _SelectTripPlanState extends State<SelectTripPlan> {
                         value: value,
                         child: Container(
                             color: Colors.white,
-                            child: Text('         ' + value + '      ')),
+                            child: Text('         $value      ')),
                       );
                     },
                   ).toList(),
