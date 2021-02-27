fdfdbca4200e59ea93e8fbeab9caa1820c4ea41a
Mina Kang <kasong13@unist.ac.kr>
2021-02-28 01:59:47 +0900

    Implemented progress bar
    - Using LinearProgressIndicator()
    - colors need to be considered

--- prototype2021/lib/survey/survey_page.dart
+++ prototype2021/lib/survey/survey_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prototype2021/survey/templates/ticket_button.dart';
 import 'package:prototype2021/survey/templates/select_trip_plan.dart';
 import 'package:prototype2021/survey/templates/story_explanation.dart';
+import 'package:prototype2021/templates/borded_button.dart';
 
 class SurveyPage extends StatefulWidget {
   @override
@@ -29,7 +30,35 @@ class _SurveyPageState extends State<SurveyPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: SelectTripPlan(),
+        child: Column(
+          children: [
+            SelectTripPlan(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                BordedButton(
+                  child: Text("  선택완료  "),
+                  onPressed: () {},
+                  radius: 30,
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5),
+              width: 350,
+              height: 15,
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: LinearProgressIndicator(
+                  value: 0.3,
+                  minHeight: 12,
+                  backgroundColor: Colors.grey[100],
+                  valueColor: new AlwaysStoppedAnimation<Color>(Colors.black),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
