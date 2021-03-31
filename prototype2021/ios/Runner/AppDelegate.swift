5a403af1a7095c5597cd11b778e81564394fd46c
준화 <junwha0511@naver.com>
2021-04-01 03:45:05 +0900

    Android and IOS setting
    -about Naver Map API
    -IOS not really completed (Mac OS needed)

--- prototype2021/ios/Runner/AppDelegate.swift
+++ prototype2021/ios/Runner/AppDelegate.swift
@@ -8,6 +8,16 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    if (CLLocationManager.locationServicesEnabled()) {
+    switch CLLocationManager.authorizationStatus() {
+    case .denied, .notDetermined, .restricted:
+        self.manager.requestAlwaysAuthorization()
+        break
+    default:
+        break
+    }
+}
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  
 }
