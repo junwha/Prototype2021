0ef388cf00d6c1354e87d6fd8e4c79b626c91398
준화 <junwha0511@naver.com>
2021-04-01 02:43:35 +0900

    Create MapView

---
+++ prototype2021/lib/event/map_view.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Colors.white,
+        leading: FlatButton(
+          color: Colors.white,
+          child: Icon(Icons.arrow_back_ios_sharp, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        titleSpacing: 10,
+        title: Text(
+          '내 주변 이벤트',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w900,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: Container(),
+    );
+  }
+}
