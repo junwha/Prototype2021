6d26260da8df4687692ff593ba200e2e51f5ca0f
Mina Kang <kasong13@unist.ac.kr>
2021-04-01 17:42:19 +0900

    Checkbox errors were fixed
    - Implemented UI that appears when both "ischecked1,2" are true.
    - Implemented DropDownButton.
    - A template for selecting a travel start date and a travel end date must be implemented.

--- prototype2021/lib/event/editor/editor_view.dart
+++ prototype2021/lib/event/editor/editor_view.dart
@@ -6,11 +6,13 @@ class EditorView extends StatefulWidget {
 }
 
 class _EditorViewState extends State<EditorView> {
+  var _isChecked1 = false;
+  var _isChecked2 = false;
+  final _valueList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  var _selectedValue = '0';
+
   @override
   Widget build(BuildContext context) {
-    var _isChecked1 = false;
-    var _isChecked2 = false;
-
     return Scaffold(
       appBar: AppBar(
         title: Text("MINAMONA"),
@@ -38,7 +40,7 @@ class _EditorViewState extends State<EditorView> {
                                 fontWeight: FontWeight.normal, fontSize: 14)),
                         Checkbox(
                           value: _isChecked1,
-                          onChanged: (value) {
+                          onChanged: (bool value) {
                             setState(() {
                               _isChecked1 = value;
                             });
@@ -49,7 +51,7 @@ class _EditorViewState extends State<EditorView> {
                                 fontWeight: FontWeight.normal, fontSize: 14)),
                         Checkbox(
                           value: _isChecked2,
-                          onChanged: (value) {
+                          onChanged: (bool value) {
                             setState(() {
                               _isChecked2 = value;
                             });
@@ -69,41 +71,64 @@ class _EditorViewState extends State<EditorView> {
   }
 
   Widget _buildUI(bool _isChecked1, bool _isChecked2) {
-    final _valueList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    var _selectedValue = '0';
-
     if (_isChecked1 && _isChecked2) {
       return Column(
         children: [
           Row(
             children: [
               Text("모집인원"),
+              SizedBox(
+                width: 50,
+              ),
+              Row(
+                children: [
+                  DropdownButton(
+                    value: _selectedValue,
+                    items: _valueList.map(
+                      (value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value;
+                      });
+                    },
+                  ),
+                  Text("명")
+                ],
+              ),
             ],
           ),
-          DropdownButton(
-            value: _selectedValue,
-            items: _valueList.map(
-              (value) {
-                return DropdownMenuItem(
-                  value: value,
-                  child: Text(value),
-                );
-              },
-            ).toList(),
-            onChanged: (value) {
-              setState(() {
-                _selectedValue = value;
-              });
-            },
+          Row(
+            children: [
+              Text("여행시작일"),
+              SizedBox(
+                width: 40,
+              ),
+              Text("선택안함")
+            ],
+          ),
+          Row(
+            children: [
+              Text("여행종료일"),
+              SizedBox(
+                width: 40,
+              ),
+              Text("선택안함")
+            ],
           )
         ],
       );
     } else if (_isChecked1) {
-      return Text("메롱");
+      return Text("");
     } else if (_isChecked2) {
-      return Text("g하,,..언제끝나냐");
+      return Text("");
     } else {
-      return Text("망했따");
+      return Text("");
     }
   }
 }
