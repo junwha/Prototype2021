1eb85de7c5b488599047a2e2bbe7178a07de63f0
Mina Kang <kasong13@unist.ac.kr>
2021-04-01 16:47:37 +0900

    Connect Editor View
    -to main

--- prototype2021/lib/main.dart
+++ prototype2021/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:prototype2021/login_page.dart';
 import 'package:prototype2021/main_page.dart';
 import 'package:prototype2021/event/home_view.dart';
+import 'package:prototype2021/event/editor/editor_view.dart';
 
 void main() {
   runApp(
@@ -10,6 +11,7 @@ void main() {
       //'/main': (context) => MainPage(),
       '/main': (context) => HomeView(),
       '/mainp': (context) => MainPage(),
+      '/editor': (context) => EditorView(),
     }),
   );
 }
