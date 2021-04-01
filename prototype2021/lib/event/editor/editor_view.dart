127484c8bf6406eb61d543e64d508fd061c2a158
Mina Kang <kasong13@unist.ac.kr>
2021-04-02 00:09:21 +0900

    In case of selecting only regardless of gender
    - If only gender is selected, age selection design is added.
    - Need a process to simplify the code.

--- prototype2021/lib/event/editor/editor_view.dart
+++ prototype2021/lib/event/editor/editor_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:f_datetimerangepicker/f_datetimerangepicker.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/cupertino.dart';
 
@@ -11,13 +10,17 @@ class EditorView extends StatefulWidget {
 class _EditorViewState extends State<EditorView> {
   var _isChecked1 = false;
   var _isChecked2 = false;
-  final _valueList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-  var _selectedValue = '0';
+  final _valueList1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  final _valueList2 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  final _valueList3 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+  var _selectedValue1 = '0';
+  var _selectedValue2 = '0';
+  var _selectedValue3 = '0';
+
   String resultString;
   DateTime _chosenDateTime;
 
   void _showDatePicker(ctx) {
-    // showCupertinoModalPopup is a built-in function of the cupertino library
     showCupertinoModalPopup(
         context: ctx,
         builder: (_) => Container(
@@ -139,8 +142,8 @@ class _EditorViewState extends State<EditorView> {
               Row(
                 children: [
                   DropdownButton(
-                    value: _selectedValue,
-                    items: _valueList.map(
+                    value: _selectedValue1,
+                    items: _valueList1.map(
                       (value) {
                         return DropdownMenuItem(
                           value: value,
@@ -150,7 +153,7 @@ class _EditorViewState extends State<EditorView> {
                     ).toList(),
                     onChanged: (value) {
                       setState(() {
-                        _selectedValue = value;
+                        _selectedValue1 = value;
                       });
                     },
                   ),
@@ -167,7 +170,7 @@ class _EditorViewState extends State<EditorView> {
               ),
               CupertinoButton(
                 padding: EdgeInsets.zero,
-                child: Text('날짜 선택',
+                child: Text('날짜선택',
                     style: TextStyle(
                         fontWeight: FontWeight.normal,
                         fontSize: 14,
@@ -185,7 +188,7 @@ class _EditorViewState extends State<EditorView> {
               ),
               CupertinoButton(
                 padding: EdgeInsets.zero,
-                child: Text('날짜 선택',
+                child: Text('날짜선택',
                     style: TextStyle(
                         fontWeight: FontWeight.normal,
                         fontSize: 14,
@@ -198,7 +201,115 @@ class _EditorViewState extends State<EditorView> {
         ],
       );
     } else if (_isChecked1) {
-      return Text("");
+      return Column(
+        children: [
+          Row(children: [
+            Text("모집인원"),
+            SizedBox(
+              width: 50,
+            ),
+            Row(
+              children: [
+                DropdownButton(
+                  value: _selectedValue1,
+                  items: _valueList1.map(
+                    (value) {
+                      return DropdownMenuItem(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedValue1 = value;
+                    });
+                  },
+                ),
+                Text("명")
+              ],
+            ),
+          ]),
+          Row(
+            children: [
+              Text("나이"),
+              SizedBox(
+                width: 80,
+              ),
+              DropdownButton(
+                value: _selectedValue2,
+                items: _valueList2.map(
+                  (value) {
+                    return DropdownMenuItem(
+                      value: value,
+                      child: Text(value),
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedValue2 = value;
+                  });
+                },
+              ),
+              Text("    ~    "),
+              DropdownButton(
+                value: _selectedValue3,
+                items: _valueList3.map(
+                  (value) {
+                    return DropdownMenuItem(
+                      value: value,
+                      child: Text(value),
+                    );
+                  },
+                ).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedValue3 = value;
+                  });
+                },
+              ),
+              Text("살")
+            ],
+          ),
+          Row(
+            children: [
+              Text("시작날짜"),
+              SizedBox(
+                width: 40,
+              ),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: Text('날짜선택',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                        color: Colors.black)),
+                onPressed: () => _showDatePicker(context),
+              ),
+            ],
+          ),
+          Text(_chosenDateTime != null ? _chosenDateTime.toString() : '선택안함'),
+          Row(
+            children: [
+              Text("종료날짜"),
+              SizedBox(
+                width: 40,
+              ),
+              CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: Text('날짜선택',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 14,
+                        color: Colors.black)),
+                onPressed: () => _showDatePicker(context),
+              ),
+            ],
+          ),
+          Text(_chosenDateTime != null ? _chosenDateTime.toString() : '선택안함'),
+        ],
+      );
     } else if (_isChecked2) {
       return Text("");
     } else {
