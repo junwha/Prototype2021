b708e3f37b29537e017e5922e45bf0508dfe7891
Mina Kang <kasong13@unist.ac.kr>
2021-04-01 16:47:14 +0900

    Bug fix
    -kt

--- prototype2021/android/app/src/main/kotlin/com/example/prototype2021/MainActivity.kt
+++ prototype2021/android/app/src/main/kotlin/com/example/prototype2021/MainActivity.kt
@@ -3,13 +3,4 @@ package com.example.prototype2021
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-
-           if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-              if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                  requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-              }
-           }
-    }
 }
