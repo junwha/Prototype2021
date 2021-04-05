d80c27537d0b52687e9403a1b76e9b3f1c7735bc
준화 <junwha0511@naver.com>
2021-04-05 21:46:32 +0900

    Upgrade hierarchy

--- prototype2021/lib/main.dart
+++ prototype2021/lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:prototype2021/login_page.dart';
-import 'package:prototype2021/main_page.dart';
-import 'package:prototype2021/event/home_view.dart';
-import 'package:prototype2021/event/map/map_view.dart';
 
 void main() {
   runApp(
     //Don't touch here. route map is in 'main_page.dart'
-    MaterialApp(home: LoginPage(), routes: {
+    MaterialApp(home: FooPage(), routes: {
       //'/main': (context) => MainPage(),
-      '/main': (context) => HomeView(),
-      '/mainp': (context) => MainPage(),
-      '/map': (context) => MapView(),
     }),
   );
 }
