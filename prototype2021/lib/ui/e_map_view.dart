c5357e0c7a41267038c085350669b16a89a4ae52
준화 <junwha0511@naver.com>
2021-04-10 17:42:07 +0900

    Feat: Initialize Google Map
    - with Camera Position in UNIST (it have to be canged to dynamic location)
    - Gesture Detectors detect scroll and zoom in out

---
+++ prototype2021/lib/ui/e_map_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/rendering.dart';
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  GoogleMapController? mapController;
+
+  //initial position
+  LatLng center =
+      LatLng(35.5735, 129.1896); //TODO(junwha): change to dynamic location
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: false,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          color: Colors.white,
+          icon: Icon(Icons.arrow_back_ios_sharp, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        titleSpacing: 10,
+        title: Text(
+          '내 주변 이벤트',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w900,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          //Set initial Camera Position
+          target: center,
+          zoom: 11.0,
+        ),
+        gestureRecognizers: //Gesture Detectors
+            <Factory<OneSequenceGestureRecognizer>>{
+          Factory<OneSequenceGestureRecognizer>(
+            () => EagerGestureRecognizer(),
+          ),
+        },
+      ),
+    );
+  }
+}
