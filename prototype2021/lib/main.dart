52c2e09da23694a1542714d3d98c4e22fb3cccda
Mina Kang <kasong13@unist.ac.kr>
2021-05-01 00:33:15 +0900

    fixed the error

--- prototype2021/lib/main.dart
+++ prototype2021/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:prototype2021/ui/login_view.dart';
+import 'package:prototype2021/ui/editor_view.dart';
 
 void main() {
   runApp(
     //Don't touch here. route map is in 'main_page.dart'
-    MaterialApp(home: LoginPage(), routes: {
+    MaterialApp(home: EditorView(), routes: {
       //'/main': (context) => MainPage(),
     }),
   );
