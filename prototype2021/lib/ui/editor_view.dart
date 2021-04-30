3ed997493b6a3220be86bd6878e8970ff5b5c87c
Mina Kang <kasong13@unist.ac.kr>
2021-05-01 00:48:31 +0900

    created a popupbutton widget

--- prototype2021/lib/ui/editor_view.dart
+++ prototype2021/lib/ui/editor_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:prototype2021/theme/checkbox_row.dart';
 import 'package:prototype2021/theme/datetimepicker_column.dart';
 import 'package:prototype2021/theme/checkbox_widget.dart';
+import 'package:prototype2021/theme/pop_up.dart';
 import 'package:prototype2021/settings/constants.dart';
 
 class EditorView extends StatefulWidget {
@@ -42,106 +43,7 @@ class _EditorViewState extends State<EditorView> {
               ),
               Row(
                 children: [
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.grey, // background
-                      onPrimary: Colors.black, // foreground
-                    ),
-                    onPressed: () {
-                      showDialog(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Column(
-                                children: [
-                                  Text('임시저장'),
-                                  SizedBox(
-                                    height: 10,
-                                  ),
-                                  Container(
-                                    height: 1,
-                                    width: double.infinity,
-                                    color: Colors.grey,
-                                  ),
-                                ],
-                              ),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: [
-                                    Container(
-                                      width: 291 * pt,
-                                      height: 250 * pt,
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        children: [
-                                          Text("임시 저장하시겠습니까?",
-                                              style:
-                                                  TextStyle(fontSize: 17 * pt)),
-                                          SizedBox(
-                                            height: 15,
-                                          ),
-                                          Text('임시 저장한 글은',
-                                              style: TextStyle(
-                                                fontSize: 14 * pt,
-                                              )),
-                                          Text('\'내 정보 > 임시 저장한 글\'',
-                                              style: TextStyle(
-                                                  fontSize: 14 * pt,
-                                                  fontWeight: FontWeight.bold)),
-                                          Text('에서 볼 수 있어요.',
-                                              style:
-                                                  TextStyle(fontSize: 14 * pt))
-                                        ],
-                                      ),
-                                    ),
-                                    Container(
-                                      height: 1,
-                                      width: double.infinity,
-                                      color: Colors.grey,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              actions: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      height: 45.5 * pt,
-                                      width: 60 * pt,
-                                      child: TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text(
-                                            '취소',
-                                            style: TextStyle(fontSize: 13 * pt),
-                                          )),
-                                    ),
-                                    Container(
-                                      height: 45.5 * pt,
-                                      width: 60 * pt,
-                                      child: TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Text(
-                                            '확인',
-                                            style: TextStyle(fontSize: 13 * pt),
-                                          )),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            );
-                          });
-                    },
-                    child: Text('임시저장',
-                        style: TextStyle(
-                            fontSize: 13 * pt, fontWeight: FontWeight.bold)),
-                  ),
+                  PopButton(),
                   SizedBox(
                     width: 10 * pt,
                   ),
