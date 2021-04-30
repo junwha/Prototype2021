e6dd2f712d687bceb782806abc609db447251e54
Mina Kang <kasong13@unist.ac.kr>
2021-05-01 03:55:35 +0900

    Simplify checkbox_widget
    - Simplify the code by creating three methods of RecruitmentNumber(),AgeSelection(),GenderRecruitment()

--- prototype2021/lib/theme/checkbox_widget.dart
+++ prototype2021/lib/theme/checkbox_widget.dart
@@ -28,262 +28,155 @@ class _CheckBoxWidgetState extends State<CheckBoxWidget> {
     } else if (this.widget.isChecked1) {
       return Column(
         children: [
-          Row(children: [
-            Text("모집인원", style: TextStyle(fontSize: 13 * pt)),
-            SizedBox(
-              width: 50,
-            ),
-            Row(
-              children: [
-                DropdownButton(
-                  value: _selectedValue1,
-                  items: _valueList1.map(
-                    (value) {
-                      return DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      );
-                    },
-                  ).toList(),
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedValue1 = value == null ? "" : value;
-                    });
-                  },
-                ),
-                Text("명", style: TextStyle(fontSize: 13 * pt))
-              ],
-            ),
-          ]),
-          Row(
-            children: [
-              Text("나이", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 80,
-              ),
-              DropdownButton(
-                value: _selectedValue2,
-                items: _valueList2.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue2 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("    ~    "),
-              DropdownButton(
-                value: _selectedValue3,
-                items: _valueList3.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue3 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("살", style: TextStyle(fontSize: 13 * pt))
-            ],
-          ),
+          RecruitmentNumber(),
+          AgeSelection(),
         ],
       );
     } else if (this.widget.isChecked2) {
       return Column(
         children: [
-          Row(children: [
-            Text("모집인원", style: TextStyle(fontSize: 13 * pt)),
-            SizedBox(
-              width: 50,
-            ),
-            Row(
-              children: [
-                DropdownButton(
-                  value: _selectedValue1,
-                  items: _valueList1.map(
-                    (value) {
-                      return DropdownMenuItem(
-                        value: value,
-                        child: Text(value),
-                      );
-                    },
-                  ).toList(),
-                  onChanged: (String? value) {
-                    setState(() {
-                      _selectedValue1 = value == null ? "" : value;
-                    });
-                  },
-                ),
-                Text("명", style: TextStyle(fontSize: 13 * pt))
-              ],
-            ),
-          ]),
-          Row(
-            children: [
-              Text("모집인원", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 40,
-              ),
-              Text("남", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 10 * pt,
-              ),
-              DropdownButton(
-                value: _selectedValue2,
-                items: _valueList2.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue2 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("명", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 20 * pt,
-              ),
-              Text("여", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 10 * pt,
-              ),
-              DropdownButton(
-                value: _selectedValue3,
-                items: _valueList3.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue3 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("명", style: TextStyle(fontSize: 13 * pt))
-            ],
-          ),
+          RecruitmentNumber(),
+          GenderRecruitment(),
         ],
       );
     } else {
       return Column(
         children: [
-          Row(
-            children: [
-              Text("모집인원", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 40,
-              ),
-              Text("남", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 10 * pt,
-              ),
-              DropdownButton(
-                value: _selectedValue2,
-                items: _valueList2.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue2 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("명", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 20 * pt,
-              ),
-              Text("여", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 10 * pt,
-              ),
-              DropdownButton(
-                value: _selectedValue3,
-                items: _valueList3.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue3 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("명", style: TextStyle(fontSize: 13 * pt))
-            ],
-          ),
-          Row(
-            children: [
-              Text("나이", style: TextStyle(fontSize: 13 * pt)),
-              SizedBox(
-                width: 80,
-              ),
-              DropdownButton(
-                value: _selectedValue2,
-                items: _valueList2.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue2 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("     ~     "),
-              DropdownButton(
-                value: _selectedValue3,
-                items: _valueList3.map(
-                  (value) {
-                    return DropdownMenuItem(
-                      value: value,
-                      child: Text(value),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectedValue3 = value == null ? "" : value;
-                  });
-                },
-              ),
-              Text("살", style: TextStyle(fontSize: 13 * pt))
-            ],
-          ),
+          GenderRecruitment(),
+          AgeSelection(),
         ],
       );
     }
+  }
+
+  Row GenderRecruitment() {
+    return Row(
+      children: [
+        Text("모집인원", style: TextStyle(fontSize: 13 * pt)),
+        SizedBox(
+          width: 40,
+        ),
+        Text("남", style: TextStyle(fontSize: 13 * pt)),
+        SizedBox(
+          width: 10 * pt,
+        ),
+        DropdownButton(
+          value: _selectedValue2,
+          items: _valueList2.map(
+            (value) {
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              );
+            },
+          ).toList(),
+          onChanged: (String? value) {
+            setState(() {
+              _selectedValue2 = value == null ? "" : value;
+            });
+          },
+        ),
+        Text("명", style: TextStyle(fontSize: 13 * pt)),
+        SizedBox(
+          width: 20 * pt,
+        ),
+        Text("여", style: TextStyle(fontSize: 13 * pt)),
+        SizedBox(
+          width: 10 * pt,
+        ),
+        DropdownButton(
+          value: _selectedValue3,
+          items: _valueList3.map(
+            (value) {
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              );
+            },
+          ).toList(),
+          onChanged: (String? value) {
+            setState(() {
+              _selectedValue3 = value == null ? "" : value;
+            });
+          },
+        ),
+        Text("명", style: TextStyle(fontSize: 13 * pt))
+      ],
+    );
+  }
+
+  Row AgeSelection() {
+    return Row(
+      children: [
+        Text("나이", style: TextStyle(fontSize: 13 * pt)),
+        SizedBox(
+          width: 80,
+        ),
+        DropdownButton(
+          value: _selectedValue2,
+          items: _valueList2.map(
+            (value) {
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              );
+            },
+          ).toList(),
+          onChanged: (String? value) {
+            setState(() {
+              _selectedValue2 = value == null ? "" : value;
+            });
+          },
+        ),
+        Text("     ~     "),
+        DropdownButton(
+          value: _selectedValue3,
+          items: _valueList3.map(
+            (value) {
+              return DropdownMenuItem(
+                value: value,
+                child: Text(value),
+              );
+            },
+          ).toList(),
+          onChanged: (String? value) {
+            setState(() {
+              _selectedValue3 = value == null ? "" : value;
+            });
+          },
+        ),
+        Text("살", style: TextStyle(fontSize: 13 * pt))
+      ],
+    );
+  }
+
+  Row RecruitmentNumber() {
+    return Row(children: [
+      Text("모집인원", style: TextStyle(fontSize: 13 * pt)),
+      SizedBox(
+        width: 50,
+      ),
+      Row(
+        children: [
+          DropdownButton(
+            value: _selectedValue1,
+            items: _valueList1.map(
+              (value) {
+                return DropdownMenuItem(
+                  value: value,
+                  child: Text(value),
+                );
+              },
+            ).toList(),
+            onChanged: (String? value) {
+              setState(() {
+                _selectedValue1 = value == null ? "" : value;
+              });
+            },
+          ),
+          Text("명", style: TextStyle(fontSize: 13 * pt))
+        ],
+      ),
+    ]);
   }
 }
