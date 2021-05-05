4a49d85e4db2f5a081e8b4407b77ae2c862935e1
준화 <junwha0511@naver.com>
2021-05-05 10:22:48 +0900

    Initialize PlaceLoader

--- prototype2021/lib/model/location_model.dart
+++ prototype2021/lib/model/location_model.dart
@@ -1,26 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:prototype2021/model/map_place.dart';
 import 'package:prototype2021/ui/marker_list.dart';
 import 'package:prototype2021/ui/location.dart';
 import 'package:prototype2021/ui/content_location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationModel with ChangeNotifier {
   List<Location> locations = [
-    ContentLocation(0, "A", LatLng(35.5735, 129.1896))
+    ContentLocation(0, "A", LatLng(37.5172, 127.0473))
   ];
 
   MarkerList markerList = MarkerList();
   bool loaded = false;
+  LatLng center;
+  late PlaceLoader placeLoader;
 
-  LocationModel() {
+  LocationModel({required this.center}) {
     init();
   }
 
   void init() async {
     loaded = await markerList.loadImage();
+    this.placeLoader = PlaceLoader(center: this.center);
+    List<PlaceData> placeDataList =
+        await placeLoader.getPlaces([RESTAURANT, HOTEL, SPOT]);
+    for (PlaceData placeData in placeDataList) {
+      locations.add(ContentLocation(
+          locations.length, placeData.name, placeData.location));
+    }
+
     markerList.addMarkerList(locations);
     notifyListeners();
   }
+
+  void loadData() {}
 
   Set<Marker> get markers =>
       markerList.markerList; //TODO: consider update location with efficiency
@@ -48,4 +62,8 @@ class LocationModel with ChangeNotifier {
   bool isUpdate(LatLngBounds bounds) {
     return false;
   }
+
+  void updateMarkers() {}
+
+  void findNearByPlaces() {}
 }
