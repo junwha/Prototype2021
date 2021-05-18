596c93f7dffe9aebb37a9b3310909b2619d32855
Mina Kang <kasong13@unist.ac.kr>
2021-05-18 20:33:05 +0900

    Add fields for numbers

--- prototype2021/lib/model/editor_model.dart
+++ prototype2021/lib/model/editor_model.dart
@@ -9,12 +9,21 @@ class EditorModel with ChangeNotifier {
   bool hasAge = false;
   bool hasGender = false;
   bool articleType = EVENT;
-  EditorModel() {}
+  String recruitNumber = '0';
+  String maleRecruitNumber = '0';
+  String femaleRecruitNumber = '0';
+  String startAge = '0';
+  String endAge = '0';
 
   void printChanged() {
     print(title);
     print(content);
     print(hasAge);
     print(hasGender);
+    print(recruitNumber);
+    print(maleRecruitNumber);
+    print(femaleRecruitNumber);
+    print(startAge);
+    print(endAge);
   }
 }
