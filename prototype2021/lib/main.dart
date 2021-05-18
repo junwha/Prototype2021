9fe828766089295c00c90424f2a2951d8a0a5493
준화 <junwha0511@naver.com>
2021-05-18 19:13:07 +0900

    Update main.dart

--- prototype2021/lib/main.dart
+++ prototype2021/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:prototype2021/ui/login_view.dart';
+import 'package:prototype2021/view/e_map_view.dart';
 
 void main() {
   runApp(
     //Don't touch here. route map is in 'main_page.dart'
-    MaterialApp(home: LoginPage(), routes: {
+    MaterialApp(home: MapView(), routes: {
       //'/main': (context) => MainPage(),
     }),
   );
