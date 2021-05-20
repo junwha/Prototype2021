bbccb1d6a499a69d35e2ab4c7fe5ccb66cd4b57b
준화 <junwha0511@naver.com>
2021-05-20 15:25:06 +0900

    Add comments

--- prototype2021/lib/theme/timer_card.dart
+++ prototype2021/lib/theme/timer_card.dart
@@ -6,13 +6,14 @@ class TimerCard extends StatefulWidget {
   String title;
   String description;
   DateTime due;
-  Function? onEnd;
+  Function? onEnd; // onEnd function is called when time out
 
   TimerCard(
       {required this.title,
       required this.description,
       required this.due,
       this.onEnd});
+
   @override
   _TimerCardState createState() => _TimerCardState();
 }
@@ -20,6 +21,7 @@ class TimerCard extends StatefulWidget {
 class _TimerCardState extends State<TimerCard> {
   Duration remainTime = Duration();
   Timer? _timer;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,7 +36,7 @@ class _TimerCardState extends State<TimerCard> {
               Text("${this.widget.title}"),
               Row(
                 children: [
-                  Text("${remainTime.toString().split(".")[0]}"),
+                  Text("${remainTime.toString().split(".")[0]}"), // Converts Datetime to time format
                   SizedBox(width: 10),
                   Text("남음"),
                 ],
@@ -49,12 +51,12 @@ class _TimerCardState extends State<TimerCard> {
 
   @override
   void initState() {
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) { // Asynchronized timer function is called by 1 second
+      setState(() { // rebuild widget with newly calculated difference
         remainTime = this.widget.due.difference(DateTime.now());
       });
 
-      if (remainTime.isNegative) {
+      if (remainTime.isNegative) { // if difference is negative, stop timer and call onEnd function
         timer.cancel();
         if (this.widget.onEnd != null) {
           this.widget.onEnd!();
@@ -65,7 +67,7 @@ class _TimerCardState extends State<TimerCard> {
   }
 
   @override
-  void dispose() {
+  void dispose() { // if widget disposed unexpectly, cancel timer
     _timer?.cancel();
     super.dispose();
   }
