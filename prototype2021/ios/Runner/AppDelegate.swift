ad040327f398a3c1a3a16ab8b48d37fc887d680d
준화 <junwha0511@naver.com>
2021-05-25 15:32:03 +0900

    Rebuild IOS dependencies
    - now IOS can run our app
    - Recreated pod file
    - flutter create -i swift

--- prototype2021/ios/Runner/AppDelegate.swift
+++ prototype2021/ios/Runner/AppDelegate.swift
@@ -8,21 +8,6 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    if (CLLocationManager.locationServicesEnabled()) {
-    switch CLLocationManager.authorizationStatus() {
-    case .denied, .notDetermined, .restricted:
-        self.manager.requestAlwaysAuthorization()
-        break
-    default:
-        break
-    }
-}
-    //Google Map
-    GMSServices.provideAPIKey("AIzaSyBhcuH45NaLJEqVuqGG7EmPqPPIJq9kumc")
-    GeneratedPluginRegistrant.register(with: self)
-  
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    
   }
-  
 }
