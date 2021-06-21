515d5e05472d83f86c4c72944840edc777bcce95
Mina Kang <kasong13@unist.ac.kr>
2021-06-22 01:03:00 +0900

    Update event_main_view.dart
    -Implement announcement part on top
    -When the "More" button is clicked, it moves to a new page
    -Implement bulletin cards to be visible on new pages

--- prototype2021/lib/ui/event_main_view.dart
+++ prototype2021/lib/ui/event_main_view.dart
@@ -86,6 +86,24 @@ class _EventMainViewState extends State<EventMainView> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Container(
+              child: Center(
+                  child: Row(
+                children: [
+                  Icon(Icons.speaker_notes_rounded),
+                  SizedBox(
+                    width: 7,
+                  ),
+                  Text(
+                    "울산광역시 불꽃축제(2021-01-04~2021-02-03",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                ],
+              )),
+              color: Colors.grey,
+              width: double.infinity,
+              height: 20 * pt,
+            ),
             Padding(
               padding:
                   const EdgeInsets.fromLTRB(15 * pt, 12 * pt, 15 * pt, 30 * pt),
@@ -243,7 +261,6 @@ class _EventMainViewState extends State<EventMainView> {
               },
             ),
             TextButton(
-                onPressed: () {},
                 child: Container(
                     height: 35 * pt,
                     width: 280 * pt,
@@ -257,10 +274,121 @@ class _EventMainViewState extends State<EventMainView> {
                           fontSize: 15 * pt,
                           fontWeight: FontWeight.bold,
                           color: Colors.black54),
-                    ))))
+                    ))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(builder: (BuildContext context) {
+                      return Second();
+                    }),
+                  );
+                })
           ],
         ),
       ),
     );
+  }
+}
+
+class Second extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          shadowColor: Colors.white,
+          leading: IconButton(
+              icon: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                  size: 35,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.people_alt_outlined,
+                  color: Colors.black,
+                  size: 35,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_none,
+                  color: Colors.black,
+                  size: 35,
+                )),
+          ],
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            RecruitCard(
+              title: '울산대 공원에서 간단히 피맥해요!',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 01, 05),
+                  start: DateTime.utc(2020, 01, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '벚꽃놀이 하러가요',
+              range: DateTimeRange(
+                  end: DateTime.utc(2021, 01, 05),
+                  start: DateTime.utc(2021, 01, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '명장스시 가실 분?',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 02, 15),
+                  start: DateTime.utc(2020, 02, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '태화강에서 치맥 하실분 구해요!',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 01, 06),
+                  start: DateTime.utc(2020, 01, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '태화강에서 치맥 하실분 구해요!',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 01, 06),
+                  start: DateTime.utc(2020, 01, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '태화강에서 치맥 하실분 구해요!',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 01, 06),
+                  start: DateTime.utc(2020, 01, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '태화강에서 치맥 하실분 구해요!',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 01, 06),
+                  start: DateTime.utc(2020, 01, 04)),
+              hasContents: false,
+            ),
+            RecruitCard(
+              title: '태화강에서 치맥 하실분 구해요!',
+              range: DateTimeRange(
+                  end: DateTime.utc(2020, 01, 06),
+                  start: DateTime.utc(2020, 01, 04)),
+              hasContents: false,
+            ),
+          ],
+        )));
   }
 }
