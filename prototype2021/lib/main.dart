85020277119ceef4729c7f95e672caa5c448201f
Mina Kang <kasong13@unist.ac.kr>
2021-06-21 21:13:52 +0900

    Update main.dart

--- prototype2021/lib/main.dart
+++ prototype2021/lib/main.dart
@@ -5,7 +5,7 @@ import 'package:prototype2021/view/e_map_view.dart';
 void main() {
   runApp(
     //Don't touch here. route map is in 'main_page.dart'
-    MaterialApp(home: MapView(), routes: {
+    MaterialApp(home: EditorView(), routes: {
       //'/main': (context) => MainPage(),
     }),
   );
