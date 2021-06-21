cc1ba9c11b907b85ffd87139317ea97bd006d9e6
준화 <junwha0511@naver.com>
2021-06-22 03:32:13 +0900

    Add placeHolder as constant

--- prototype2021/lib/settings/constants.dart
+++ prototype2021/lib/settings/constants.dart
@@ -1,2 +1,4 @@
 const double pt = 96 / 72;
 const kGoogleApiKey = "AIzaSyBhcuH45NaLJEqVuqGG7EmPqPPIJq9kumc";
+const String placeHolder =
+    "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg";
