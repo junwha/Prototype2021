2a647e1c058763da7e4b84d781b1f7edf65fb18c
준화 <junwha0511@naver.com>
2021-06-23 09:16:53 +0900

    Refactor: extract methods

--- prototype2021/lib/ui/event_main_view.dart
+++ prototype2021/lib/ui/event_main_view.dart
@@ -19,273 +19,299 @@ class _EventMainViewState extends State<EventMainView> {
   ];
   int _pageIndex = 0;
   double image_index = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        shadowColor: Colors.white,
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-          onPressed: () {},
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.search,
-                color: Colors.black,
-                size: 35,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.people_alt_outlined,
-                color: Colors.black,
-                size: 35,
-              )),
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.notifications_none,
-                color: Colors.black,
-                size: 35,
-              )),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: (value) {
-          setState(() {
-            _pageIndex = value;
-          });
-        },
-        currentIndex: _pageIndex,
-        selectedItemColor: Colors.blue,
-        unselectedItemColor: Colors.black26,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.people_alt_sharp, size: 40), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home_outlined,
-                size: 40,
-              ),
-              label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.people,
-                size: 40,
-              ),
-              label: ""),
-        ],
-      ),
+      appBar: buildAppBar(),
+      bottomNavigationBar: buildBottomNavigationBar(),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            Container(
-              child: Center(
-                  child: Row(
-                children: [
-                  Icon(Icons.speaker_notes_rounded),
-                  SizedBox(
-                    width: 7,
-                  ),
-                  Text(
-                    "울산광역시 불꽃축제(2021-01-04~2021-02-03",
-                    style: TextStyle(fontSize: 17),
-                  ),
-                ],
-              )),
-              color: Colors.grey,
-              width: double.infinity,
-              height: 20 * pt,
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.fromLTRB(15 * pt, 12 * pt, 15 * pt, 30 * pt),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Text(
-                            "현재 위치",
-                            style: TextStyle(
-                                fontSize: 20 * pt, fontWeight: FontWeight.bold),
-                          ),
-                          Icon(
-                            Icons.arrow_drop_down_outlined,
-                            size: 40,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Text("지도 보기",
-                              style: TextStyle(
-                                  fontSize: 17 * pt,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey)),
-                          Icon(Icons.map_sharp, size: 25),
-                        ],
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 6 * pt,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          SelectableTextButton(
-                              titleName: "내 주변 이벤트",
-                              isChecked: isChecked[0],
-                              onPressed: () {
-                                setState(() {
-                                  isChecked[1] = false;
-                                  isChecked[0] = true;
-                                });
-                              }),
-                          SizedBox(width: 10),
-                          SelectableTextButton(
-                              titleName: "동행찾기",
-                              isChecked: isChecked[1],
-                              onPressed: () {
-                                setState(() {
-                                  isChecked[1] = true;
-                                  isChecked[0] = false;
-                                });
-                              }),
-                        ],
-                      ),
-                      Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Icon(
-                              Icons.view_array_outlined,
-                              size: 38,
-                            ),
-                            Icon(
-                              Icons.departure_board_outlined,
-                              size: 32,
-                            )
-                          ]),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Stack(
-              alignment: Alignment.bottomCenter,
-              children: [
-                CarouselSlider(
-                    options: CarouselOptions(
-                      onPageChanged: (i, reason) {
-                        setState(() {
-                          image_index = i.toDouble();
-                        });
-                      },
-                      height: 200,
-                      viewportFraction: 1,
-                    ),
-                    items: images.map((url) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Container(
-                            width: MediaQuery.of(context).size.width,
-                            child: Image.network(
-                              url,
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        },
-                      );
-                    }).toList()),
-                DotsIndicator(
-                  dotsCount: images.length,
-                  position: image_index,
-                )
-              ],
-            ),
+            buildTopNotice(),
+            buildSelectSection(),
+            buildImageArea(),
             SizedBox(
               height: 30,
             ),
-            Container(
-              width: double.infinity,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "마감 임박 게시글",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14 * pt),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            TimerCard(
-              title: "태화강 근처",
-              description: "낚시하실분 ㅎㅎ",
-              due: DateTime(2021, 5, 24, 6, 23, 00),
-              onEnd: () {
-                print("asdf");
-              },
-            ),
-            TimerCard(
-              title: "울산대 공원에서 피맥해요!",
-              description: "경치보면서 같이 힐링해요 ㅎㅎ잘 먹는 분이면 더 좋습니다!",
-              due: DateTime(2021, 5, 24, 6, 23, 00),
-              onEnd: () {
-                print("asdf");
-              },
-            ),
-            TimerCard(
-              title: "울산 불꽃 축제 같이 가실분?",
-              description: "사진 찍고 같이 선물 받아요~~!",
-              due: DateTime(2021, 5, 24, 6, 23, 00),
-              onEnd: () {
-                print("asdf");
-              },
-            ),
-            TextButton(
-                child: Container(
-                    height: 35 * pt,
-                    width: 280 * pt,
-                    decoration: BoxDecoration(
-                        border: Border.all(width: 1),
-                        borderRadius: BorderRadius.all(Radius.circular(10))),
-                    child: Center(
-                        child: Text(
-                      "더보기",
-                      style: TextStyle(
-                          fontSize: 15 * pt,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black54),
-                    ))),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<void>(builder: (BuildContext context) {
-                      return Second();
-                    }),
-                  );
-                })
+            buildArticleList(context)
           ],
         ),
       ),
+    );
+  }
+
+  Column buildArticleList(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "마감 임박 게시글",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14 * pt),
+                ),
+              ],
+            ),
+          ),
+        ),
+        TimerCard(
+          title: "태화강 근처",
+          description: "낚시하실분 ㅎㅎ",
+          due: DateTime(2021, 5, 24, 6, 23, 00),
+          onEnd: () {
+            print("asdf");
+          },
+        ),
+        TimerCard(
+          title: "울산대 공원에서 피맥해요!",
+          description: "경치보면서 같이 힐링해요 ㅎㅎ잘 먹는 분이면 더 좋습니다!",
+          due: DateTime(2021, 5, 24, 6, 23, 00),
+          onEnd: () {
+            print("asdf");
+          },
+        ),
+        TimerCard(
+          title: "울산 불꽃 축제 같이 가실분?",
+          description: "사진 찍고 같이 선물 받아요~~!",
+          due: DateTime(2021, 5, 24, 6, 23, 00),
+          onEnd: () {
+            print("asdf");
+          },
+        ),
+        TextButton(
+            child: Container(
+                height: 35 * pt,
+                width: 280 * pt,
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1),
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Center(
+                    child: Text(
+                  "더보기",
+                  style: TextStyle(
+                      fontSize: 15 * pt,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54),
+                ))),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute<void>(builder: (BuildContext context) {
+                  return Second();
+                }),
+              );
+            }),
+      ],
+    );
+  }
+
+  Stack buildImageArea() {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        CarouselSlider(
+            options: CarouselOptions(
+              onPageChanged: (i, reason) {
+                setState(() {
+                  image_index = i.toDouble();
+                });
+              },
+              height: 200,
+              viewportFraction: 1,
+            ),
+            items: images.map((url) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.network(
+                      url,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              );
+            }).toList()),
+        DotsIndicator(
+          dotsCount: images.length,
+          position: image_index,
+        )
+      ],
+    );
+  }
+
+  Padding buildSelectSection() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15 * pt, 12 * pt, 15 * pt, 30 * pt),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "현재 위치",
+                    style: TextStyle(
+                        fontSize: 20 * pt, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    Icons.arrow_drop_down_outlined,
+                    size: 40,
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Text("지도 보기",
+                      style: TextStyle(
+                          fontSize: 17 * pt,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey)),
+                  Icon(Icons.map_sharp, size: 25),
+                ],
+              )
+            ],
+          ),
+          SizedBox(
+            height: 6 * pt,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SelectableTextButton(
+                      titleName: "내 주변 이벤트",
+                      isChecked: isChecked[0],
+                      onPressed: () {
+                        setState(() {
+                          isChecked[1] = false;
+                          isChecked[0] = true;
+                        });
+                      }),
+                  SizedBox(width: 10),
+                  SelectableTextButton(
+                      titleName: "동행찾기",
+                      isChecked: isChecked[1],
+                      onPressed: () {
+                        setState(() {
+                          isChecked[1] = true;
+                          isChecked[0] = false;
+                        });
+                      }),
+                ],
+              ),
+              Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Icon(
+                  Icons.view_array_outlined,
+                  size: 38,
+                ),
+                Icon(
+                  Icons.departure_board_outlined,
+                  size: 32,
+                )
+              ]),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildTopNotice() {
+    return Container(
+      child: Center(
+          child: Row(
+        children: [
+          Icon(Icons.speaker_notes_rounded),
+          SizedBox(
+            width: 7,
+          ),
+          Text(
+            "울산광역시 불꽃축제(2021-01-04~2021-02-03",
+            style: TextStyle(fontSize: 17),
+          ),
+        ],
+      )),
+      color: Colors.grey,
+      width: double.infinity,
+      height: 20 * pt,
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      onTap: (value) {
+        setState(() {
+          _pageIndex = value;
+        });
+      },
+      currentIndex: _pageIndex,
+      selectedItemColor: Colors.blue,
+      unselectedItemColor: Colors.black26,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon: Icon(Icons.people_alt_sharp, size: 40), label: ""),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              size: 40,
+            ),
+            label: ""),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.people,
+              size: 40,
+            ),
+            label: ""),
+      ],
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      shadowColor: Colors.white,
+      leading: IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
+        onPressed: () {},
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.black,
+              size: 35,
+            )),
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.people_alt_outlined,
+              color: Colors.black,
+              size: 35,
+            )),
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_none,
+              color: Colors.black,
+              size: 35,
+            )),
+      ],
     );
   }
 }
