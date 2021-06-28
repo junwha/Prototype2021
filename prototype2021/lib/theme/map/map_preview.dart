bfbc4cb3913818f27033d0f846d1d767e9815e84
준화 <junwha0511@naver.com>
2021-06-28 22:25:10 +0900

    Implement map_preview

---
+++ prototype2021/lib/theme/map/map_preview.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:prototype2021/model/map/location.dart';
+import 'package:prototype2021/model/map/location_model.dart';
+import 'package:prototype2021/model/map/map_place.dart';
+import 'package:prototype2021/theme/map/marker.dart';
+
+class MapPreview extends StatefulWidget {
+  LatLng center;
+
+  MapPreview({required this.center});
+  @override
+  _MapPreviewState createState() => _MapPreviewState();
+}
+
+class _MapPreviewState extends State<MapPreview> {
+  GoogleMapController? mapController;
+
+  //Save positions of last tapped and pressed
+  // LatLng? _lastTap;
+  // LatLng? _lastLongPress;
+  // TODO(junwha): after all test, place marks here
+  MarkerList markerList = MarkerList();
+  bool isLoaded = true;
+  void _onMapCreated(GoogleMapController controller) async {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    load();
+  }
+
+  void load() async {
+    // TODO(junwha): locaiton model 상속으로 GooglePlace 관련 메소드 분리하기
+    isLoaded = await markerList.loadImage();
+    setState(() {
+      markerList
+          .addMarker(Location(this.widget.center, PlaceType.DEFAULT, "T"));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return !isLoaded
+        ? Text("Loading...")
+        : SizedBox(
+            height: 200,
+            child: GoogleMap(
+              zoomControlsEnabled: false,
+              scrollGesturesEnabled: true,
+              rotateGesturesEnabled: false,
+              tiltGesturesEnabled: false,
+              myLocationButtonEnabled: false,
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                //Set initial Camera Position
+                target: this.widget.center,
+                zoom: 15.0,
+              ),
+              gestureRecognizers: //Gesture Detectors
+                  <Factory<OneSequenceGestureRecognizer>>{
+                Factory<OneSequenceGestureRecognizer>(
+                  () => EagerGestureRecognizer(),
+                ),
+              },
+              markers: markerList.markerList,
+            ),
+          );
+  }
+}
