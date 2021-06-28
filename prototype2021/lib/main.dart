63f828cfd142531c0cd68c71e937f66b70326183
준화 <junwha0511@naver.com>
2021-06-29 00:36:11 +0900

    Update main.dart

--- prototype2021/lib/main.dart
+++ prototype2021/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:prototype2021/ui/editor_view.dart';
 import 'package:prototype2021/ui/map_view.dart';
+import 'package:prototype2021/ui/select_location_view.dart';
 
 void main() {
   runApp(
     //Don't touch here. route map is in 'main_page.dart'
-    MaterialApp(home: MapView(), routes: {
+    MaterialApp(home: EditorView(), routes: {
+      'select_location': (context) => SelectLocationView(),
       //'/main': (context) => MainPage(),
     }),
   );
