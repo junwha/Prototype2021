1cb9ebc203f632d0b70b951265ccfe48e71e825e
준화 <junwha0511@naver.com>
2021-06-29 04:55:43 +0900

    Update placeholder

--- prototype2021/lib/settings/constants.dart
+++ prototype2021/lib/settings/constants.dart
@@ -8,4 +8,4 @@ const String ENROL_RECRUITMENTS_EVENT_API =
     'http://api.tripbuilder.co.kr/recruitments/events/';
 const kGoogleApiKey = "AIzaSyBhcuH45NaLJEqVuqGG7EmPqPPIJq9kumc";
 const String placeHolder =
-    "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg";
+    "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/96235907_114467910254216_5280857475220766720_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=973b4a&_nc_ohc=gSDB_pgPMDoAX9qQhVM&_nc_ht=scontent-ssn1-1.xx&oh=28b84509d47ce5fc2ac40f702e601649&oe=60DE8F93";
