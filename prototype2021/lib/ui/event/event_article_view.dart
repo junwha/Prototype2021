709bb282b44a3a8fc11bc29304eef9a192bb7cf9
준화 <junwha0511@naver.com>
2021-07-03 22:35:24 +0900

    Fix: map crash because of use after dispose

--- prototype2021/lib/ui/event/event_article_view.dart
+++ prototype2021/lib/ui/event/event_article_view.dart
@@ -1,6 +1,8 @@
 import 'package:prototype2021/model/event_article_model.dart';
+import 'package:prototype2021/settings/constants.dart';
 import 'package:prototype2021/theme/cards/recruit_card.dart';
 import 'package:flutter/material.dart';
+import 'package:prototype2021/theme/selectable_text_button.dart';
 import 'package:prototype2021/ui/event/event_detail_view.dart';
 import 'package:provider/provider.dart';
 
@@ -12,16 +14,34 @@ class EventArticleView extends StatefulWidget {
 }
 
 class _EventArticleViewState extends State<EventArticleView> {
+  List<bool> isChecked = [true, false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: buildAppBar(context),
       body: ChangeNotifierProvider(
         create: (context) => EventArticleModel(),
         child: Consumer(
             builder: (context, EventArticleModel eventArticlesModel, child) {
-          return SingleChildScrollView(
-              child: buildArticles(eventArticlesModel));
+          return Stack(
+            children: [
+              SingleChildScrollView(
+                child: buildArticles(eventArticlesModel),
+              ),
+              Container(
+                color: Colors.white,
+                height: 170,
+                child: Column(
+                  children: [
+                    buildTopNotice(),
+                    buildSelectSection(), // 현재 위치, 지도보기 / 내 주변 이벤트, 동행 찾기
+                  ],
+                ),
+              ),
+            ],
+          );
         }),
       ),
     );
@@ -29,28 +49,31 @@ class _EventArticleViewState extends State<EventArticleView> {
 
   Widget buildArticles(EventArticleModel eventArticlesModel) {
     if (eventArticlesModel.isEventArticleLoading) return Text("Loading ...");
-    return Column(
-        children: eventArticlesModel.eventArticleList
-            .map(
-              (e) => GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<void>(builder: (BuildContext context) {
-                      return EventDetailView(e.id);
-                    }),
-                  );
-                },
-                child: RecruitCard(
-                  title: e.title,
-                  hasContents: false,
-                  range: e.period,
-                  heartCount: e.hearts,
-                  commentCount: e.comments,
+    return Padding(
+      padding: const EdgeInsets.only(top: 170, bottom: 500),
+      child: Column(
+          children: eventArticlesModel.eventArticleList
+              .map(
+                (e) => GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute<void>(builder: (BuildContext context) {
+                        return EventDetailView(e.id);
+                      }),
+                    );
+                  },
+                  child: RecruitCard(
+                    title: e.title,
+                    hasContents: false,
+                    range: e.period,
+                    heartCount: e.hearts,
+                    commentCount: e.comments,
+                  ),
                 ),
-              ),
-            )
-            .toList());
+              )
+              .toList()),
+    );
   }
 
   AppBar buildAppBar(BuildContext context) {
@@ -88,6 +111,128 @@ class _EventArticleViewState extends State<EventArticleView> {
               size: 35,
             )),
       ],
+    );
+  }
+
+  Padding buildSelectSection() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(15 * pt, 12 * pt, 15 * pt, 0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    "현재 위치",
+                    style: TextStyle(
+                        fontSize: 20 * pt, fontWeight: FontWeight.bold),
+                  ),
+                  Icon(
+                    Icons.arrow_drop_down_outlined,
+                    size: 40,
+                  ),
+                ],
+              ),
+              TextButton(
+                child: Row(
+                  children: [
+                    Text("지도 보기",
+                        style: TextStyle(
+                            fontSize: 17 * pt,
+                            fontWeight: FontWeight.bold,
+                            color: Color.fromRGBO(85, 85, 85, 1))),
+                    SizedBox(
+                      width: 6,
+                    ),
+                    Image.asset(
+                      "assets/icons/map.png",
+                      width: 25,
+                      height: 25,
+                    )
+                  ],
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, "map");
+                },
+              )
+            ],
+          ),
+          SizedBox(
+            height: 6 * pt,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  SelectableTextButton(
+                      titleName: "내 주변 이벤트",
+                      isChecked: isChecked[0],
+                      onPressed: () {
+                        setState(() {
+                          isChecked[1] = false;
+                          isChecked[0] = true;
+                        });
+                      }),
+                  SizedBox(width: 10),
+                  SelectableTextButton(
+                      titleName: "동행찾기",
+                      isChecked: isChecked[1],
+                      onPressed: () {
+                        setState(() {
+                          isChecked[1] = true;
+                          isChecked[0] = false;
+                        });
+                      }),
+                ],
+              ),
+              Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                IconButton(
+                  icon: Image.asset(
+                    "assets/icons/filter_list_24px.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Image.asset(
+                    "assets/icons/editor.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, "editor");
+                  },
+                )
+              ]),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container buildTopNotice() {
+    return Container(
+      child: Center(
+          child: Row(
+        children: [
+          Image.asset("assets/icons/message_outlined.png"),
+          SizedBox(
+            width: 7,
+          ),
+          Text(
+            "울산광역시 불꽃축제(2021-01-04~2021-02-03",
+            style: TextStyle(fontSize: 17),
+          ),
+        ],
+      )),
+      color: Color.fromRGBO(219, 219, 219, 1),
+      width: double.infinity,
+      height: 20 * pt,
     );
   }
 }
