3f9dd98f8ec98bde54bb2cde72673286e529602d
준화 <junwha0511@naver.com>
2021-07-05 18:40:44 +0900

    Erase enrolled route

--- prototype2021/lib/ui/event/event_main_view.dart
+++ prototype2021/lib/ui/event/event_main_view.dart
@@ -6,6 +6,7 @@ import 'package:prototype2021/theme/cards/timer_card.dart';
 import 'package:prototype2021/theme/event_articles.dart';
 import 'package:prototype2021/theme/selectable_text_button.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:prototype2021/ui/event/editor_view.dart';
 import 'package:prototype2021/ui/event/event_detail_view.dart';
 import 'package:prototype2021/ui/event/event_search_view.dart';
 import 'package:prototype2021/ui/event/my_page_view.dart';
@@ -263,7 +264,10 @@ class _EventMainViewState extends State<EventMainView> {
                     height: 40,
                   ),
                   onPressed: () {
-                    Navigator.pushNamed(context, "editor");
+                    Navigator.push(context, MaterialPageRoute<void>(
+                        builder: (BuildContext context) {
+                      return EditorView();
+                    }));
                   },
                 )
               ]),
