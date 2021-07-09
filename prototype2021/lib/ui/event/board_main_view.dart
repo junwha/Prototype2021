6c05be647ddaf65adbc9a8401932026b2d5ad3e4
준화 <junwha0511@naver.com>
2021-07-10 04:43:17 +0900

    Implement top section of board main

--- prototype2021/lib/ui/event/board_main_view.dart
+++ prototype2021/lib/ui/event/board_main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prototype2021/settings/constants.dart';
 import 'package:prototype2021/ui/event/my_page_view.dart';
 
 class BoardMainView extends StatefulWidget {
@@ -12,15 +13,53 @@ class _BoardMainViewState extends State<BoardMainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: buildAppBar(),
-      body: Container(
-        child: Text("asdf"),
+      body: DefaultTabController(
+        initialIndex: 1,
+        length: 2,
+        child: Container(
+          child: Column(
+            children: [
+              buildCurrentLocation(),
+              TabBar(
+                unselectedLabelColor: Color(0xffbdbdbd),
+                labelColor: Colors.black,
+                indicatorColor: Colors.black,
+                indicatorWeight: 1 * pt,
+                tabs: [
+                  Tab(
+                    child: Text(
+                      '플랜',
+                      style: TextStyle(
+                        fontSize: 15 * pt,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      '컨텐츠',
+                      style: TextStyle(
+                        fontSize: 15 * pt,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
+      elevation: 0,
       backgroundColor: Colors.white,
       shadowColor: Colors.white,
       leading: IconButton(
@@ -48,4 +87,41 @@ class _BoardMainViewState extends State<BoardMainView> {
       ],
     );
   }
+}
+
+Widget buildCurrentLocation() {
+  return Padding(
+    padding: const EdgeInsets.fromLTRB(15 * pt, 12 * pt, 15 * pt, 29 * pt),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        TextButton(
+          child: Row(
+            children: [
+              Text(
+                '국내 전체',
+                style: TextStyle(
+                  color: Color(0xff444444),
+                  fontSize: 24 * pt,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(width: 10 * pt),
+              ImageIcon(
+                AssetImage("assets/icons/ic_area_arrow_down_unfold.png"),
+                color: Colors.black,
+                size: 14 * pt,
+              ),
+            ],
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Image.asset("assets/icons/ic_filter_gray.png"),
+        ),
+      ],
+    ),
+  );
 }
