ffd0e729de628e1c65aa22174eca4eea6ef923bd
Mina Kang <kasong13@unist.ac.kr>
2021-07-16 02:48:55 +0900

    Implement login.settingBirth.0
    Implement login.settingBirth.0
    - Using Dropdownbutton

--- prototype2021/lib/ui/event/signin_page/signin_view_4.dart
+++ prototype2021/lib/ui/event/signin_page/signin_view_4.dart
@@ -11,6 +11,79 @@ class SigninView4 extends StatefulWidget {
 }
 
 class _SigninView4State extends State<SigninView4> {
+  int startAge = 1999;
+  String startMonth = '1월';
+  int startDay = 1;
+  final List<int> dayList = [
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31
+  ];
+  final List<String> monthList = [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월'
+  ];
+  final List<int> ageList = [
+    1990,
+    1991,
+    1992,
+    1993,
+    1994,
+    1995,
+    1996,
+    1997,
+    1998,
+    1999,
+    2000,
+    2001,
+    2002,
+    2003,
+    2004,
+    2005,
+    2006,
+    2007,
+    2008,
+    2009,
+    2010
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +104,134 @@ class _SigninView4State extends State<SigninView4> {
         ),
         SizedBox(
           height: 40,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '생년월일',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xff999999),
+                  fontFamily: 'Roboto',
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Color(0xffbdbdbd),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    height: 60,
+                    width: 120,
+                    child: Center(
+                      child: DropdownButton<int>(
+                        value: startAge,
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        items: ageList.map(
+                          (value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text("$value"),
+                            );
+                          },
+                        ).toList(),
+                        onChanged: (int? value) {
+                          setState(() {
+                            startAge = value == null ? 0 : value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Color(0xffbdbdbd),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    height: 60,
+                    width: 120,
+                    child: Center(
+                      child: DropdownButton<String>(
+                        value: startMonth,
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        items: monthList.map(
+                          (value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text("$value"),
+                            );
+                          },
+                        ).toList(),
+                        onChanged: (String? value) {
+                          setState(() {
+                            startMonth = value == null ? '1월' : value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Color(0xffbdbdbd),
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    height: 60,
+                    width: 120,
+                    child: Center(
+                      child: DropdownButton<int>(
+                        value: startDay,
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        items: dayList.map(
+                          (value) {
+                            return DropdownMenuItem(
+                              value: value,
+                              child: Text("$value"),
+                            );
+                          },
+                        ).toList(),
+                        onChanged: (int? value) {
+                          setState(() {
+                            startDay = value == null ? 1 : value;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ])),
     );
