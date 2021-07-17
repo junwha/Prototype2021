7517df47d305860c4d0b48fb67efbcaac45bff8c
Mina Kang <kasong13@unist.ac.kr>
2021-07-18 03:49:56 +0900

    Update board_detail_view.dart
    -Implement buildPriceArea()
    - zeplin 위젯의 색깔 오류
    - 임의의 색깔로 지정하여 구현

--- prototype2021/lib/ui/event/board_detail_view.dart
+++ prototype2021/lib/ui/event/board_detail_view.dart
@@ -19,109 +19,88 @@ class BoardDetailViewState extends State<BoardDetailView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(23.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '울산대공원',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 21,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w700,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '울산대공원',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 21,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Image.asset(
-                            'assets/icons/ic_main_heart_default.png'))
-                  ],
-                ),
-                Text(
-                  '대한민국, 울산',
-                  style: TextStyle(
-                    color: Color(0xff707070),
-                    fontSize: 13,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w700,
+                      IconButton(
+                          onPressed: () {},
+                          icon: Image.asset(
+                              'assets/icons/ic_main_heart_default.png'))
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Row(
-                  children: [
-                    Image.asset('assets/icons/ic_pc_star_big.png'),
-                    SizedBox(
-                      width: 5,
+                  Text(
+                    '대한민국, 울산',
+                    style: TextStyle(
+                      color: Color(0xff707070),
+                      fontSize: 13,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w700,
                     ),
-                    Text(
-                      '3.7 (369)',
-                      style: TextStyle(
-                        color: Color(0xff707070),
-                        fontSize: 15,
-                        fontFamily: 'Roboto',
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset('assets/icons/ic_pc_star_big.png'),
+                      SizedBox(
+                        width: 5,
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                buildTags(),
-                SizedBox(
-                  height: 18,
-                ),
-                buildImageArea(),
-                SizedBox(
-                  height: 8,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 1,
-            color: Color(0xffe8e8e8),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(23.0),
-            child: Column(
-              children: [
-                Text(
-                  '다양한 놀이 기구와 운동 시설을 갖춘 도심 공원, 울산대공원',
-                  style: TextStyle(
-                    height: 1.3,
-                    color: Color(0xff010101),
-                    fontSize: 21,
-                    letterSpacing: 1.5,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w700,
+                      Text(
+                        '3.7 (369)',
+                        style: TextStyle(
+                          color: Color(0xff707070),
+                          fontSize: 15,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Text(
-                  '울산대공원은 울산광역시 남구 대공원로 94에 있으며, 옥동과 신정동에 걸쳐 있는 도심 공원이다. 전체 면적은 약364만여m2이고, 시설 면적은 79만m2이다. 공원 정문 쪽에는 소규모 워터파크와 수영장 시설을 갖추고 있는 아쿠아시스가 입지하고 있다. 공원 정문과 동문 사이에 현충탑이 자리잡고 있으며, 매년 6월 6일에 현충탑 앞에서 추념식이 열린다. 정문, 남문, 동문 모두 자전거 대여소가 있어 자전거 대여를 할 수 있다.',
-                  style: TextStyle(
-                    color: Colors.black,
-                    height: 1.5,
-                    fontSize: 15,
-                    fontFamily: 'Roboto',
+                  SizedBox(
+                    height: 6,
                   ),
-                ),
-              ],
+                  buildTags(),
+                  SizedBox(
+                    height: 18,
+                  ),
+                  buildImageArea(),
+                  SizedBox(
+                    height: 8,
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            Container(
+              width: double.infinity,
+              height: 1,
+              color: Color(0xffe8e8e8),
+            ),
+            buildTextArea(),
+            Container(
+              width: double.infinity,
+              height: 1,
+              color: Color(0xffe8e8e8),
+            ),
+            buildPriceArea()
+          ],
+        ),
       ),
     );
   }
@@ -241,6 +220,88 @@ class BoardDetailViewState extends State<BoardDetailView> {
           position: image_index,
         )
       ],
+    );
+  }
+
+  Padding buildTextArea() {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        children: [
+          Text(
+            '다양한 놀이 기구와 운동 시설을 갖춘 도심 공원, 울산대공원',
+            style: TextStyle(
+              height: 1.3,
+              color: Color(0xff010101),
+              fontSize: 21,
+              letterSpacing: 1.5,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+            '울산대공원은 울산광역시 남구 대공원로 94에 있으며, 옥동과 신정동에 걸쳐 있는 도심 공원이다. 전체 면적은 약364만여m2이고, 시설 면적은 79만m2이다. 공원 정문 쪽에는 소규모 워터파크와 수영장 시설을 갖추고 있는 아쿠아시스가 입지하고 있다. 공원 정문과 동문 사이에 현충탑이 자리잡고 있으며, 매년 6월 6일에 현충탑 앞에서 추념식이 열린다. 정문, 남문, 동문 모두 자전거 대여소가 있어 자전거 대여를 할 수 있다.',
+            style: TextStyle(
+              color: Colors.black,
+              height: 1.5,
+              fontSize: 15,
+              fontFamily: 'Roboto',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Padding buildPriceArea() {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text("이용료",
+                  style: const TextStyle(
+                      color: const Color(0xff080808),
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Roboto",
+                      fontStyle: FontStyle.normal,
+                      letterSpacing: 1.5,
+                      fontSize: 17.0)),
+              SizedBox(
+                width: 4,
+              ),
+              Text(
+                "유료",
+                style: const TextStyle(
+                    color: const Color(0xff004af7),
+                    fontWeight: FontWeight.w700,
+                    fontFamily: "Roboto",
+                    fontStyle: FontStyle.normal,
+                    letterSpacing: 1.5,
+                    fontSize: 17.0),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Text(
+              '성인 20유로 (홈페이지 예약 시, 19유로) \n 만 11세 이하 무료\n사전 예매 가능 (홈페이지)\n00 카드 소지 시 30% 할인',
+              style: TextStyle(
+                color: Color(0xff707070),
+                height: 1.9,
+                fontSize: 15,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w700,
+              ),
+              textAlign: TextAlign.left),
+        ],
+      ),
     );
   }
 }
