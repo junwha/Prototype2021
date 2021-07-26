b22d83e64319f577d511e74cd6d053adde427bda
준화 <junwha0511@naver.com>
2021-07-27 00:56:23 +0900

    Create select_location_toggle_view.dart

---
+++ prototype2021/lib/ui/board/select_location_toggle_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SelectLocationToggleView extends StatefulWidget {
+  const SelectLocationToggleView({Key? key}) : super(key: key);
+
+  @override
+  _SelectLocationToggleViewState createState() =>
+      _SelectLocationToggleViewState();
+}
+
+class _SelectLocationToggleViewState extends State<SelectLocationToggleView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
