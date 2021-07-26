a8ea07eb7f55a8eeb4c8e965a9382dfca73b0dac
준화 <junwha0511@naver.com>
2021-07-27 00:55:21 +0900

    Create location toggle widget

---
+++ prototype2021/lib/theme/location_toggle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationToggle extends StatefulWidget {
+  const LocationToggle({Key? key}) : super(key: key);
+
+  @override
+  _LocationToggleState createState() => _LocationToggleState();
+}
+
+class _LocationToggleState extends State<LocationToggle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
