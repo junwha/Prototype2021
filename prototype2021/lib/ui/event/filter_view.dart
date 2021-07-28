11d6f740e493984b6bcc1c1d8b9c97ecd08b9adf
Mina Kang <kasong13@unist.ac.kr>
2021-07-29 03:56:01 +0900

    Update filter_view.dart
    +,- 버튼을 눌렀을 때 기간을 설정할 수 있도록 구현
    - pluscolor, minuscolor라는 필드를 true로 설정하고 각각 +버튼, -버튼을 눌렀을 경우 max ,혹은 min값에서 color의 bool 형이 false로 바뀌도록 구현
    - 버튼을 누르기 전까지 아이콘이 변화하지 않은 문제점을 해결해야한다.

--- prototype2021/lib/ui/event/filter_view.dart
+++ prototype2021/lib/ui/event/filter_view.dart
@@ -38,6 +38,43 @@ class _FilterViewState extends State<FilterView> {
     '여행경비높은순',
   ];
   var _selectedValue;
+  final List<String> planList = [
+    '당일치기',
+    '2일',
+    '3일',
+    '4일',
+    '5일',
+    '6일',
+    '7일~14일',
+    '15일이상'
+  ];
+  int _counter = 0;
+  bool pluscolor = true;
+  bool minuscolor = true;
+
+  void _incrementCounter() {
+    setState(() {
+      if (_counter < 7) {
+        _counter++;
+        pluscolor = true;
+      } else {
+        _counter = 7;
+        pluscolor = false;
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter == 0) {
+        _counter = 0;
+        minuscolor = false;
+      } else {
+        _counter--;
+        minuscolor = true;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -335,6 +372,65 @@ class _FilterViewState extends State<FilterView> {
                       });
                     }),
               ),
+              // 기간
+              SizedBox(
+                height: 15,
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("기간",
+                        style: const TextStyle(
+                            color: const Color(0xff000000),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: "Roboto",
+                            fontStyle: FontStyle.normal,
+                            fontSize: 16.0),
+                        textAlign: TextAlign.left),
+                    Row(
+                      children: [
+                        FloatingActionButton(
+                            mini: true,
+                            backgroundColor: Colors.white,
+                            elevation: 0,
+                            onPressed: _incrementCounter,
+                            child: this.pluscolor
+                                ? Image.asset(
+                                    'assets/icons/button_filter_plus_black.png')
+                                : Image.asset(
+                                    'assets/icons/button_filter_plus_gray.png')),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Text(
+                          planList[_counter],
+                          style: const TextStyle(
+                              color: const Color(0xff707070),
+                              fontWeight: FontWeight.w400,
+                              fontFamily: "Roboto",
+                              fontStyle: FontStyle.normal,
+                              fontSize: 14.0),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        FloatingActionButton(
+                            mini: true,
+                            backgroundColor: Colors.white,
+                            elevation: 0,
+                            onPressed: _decrementCounter,
+                            child: this.minuscolor
+                                ? Image.asset(
+                                    'assets/icons/button_filter_minus_black.png')
+                                : Image.asset(
+                                    'assets/icons/button_filter_minus_gray.png')),
+                      ],
+                    ),
+                  ])
             ],
           ),
         ));
