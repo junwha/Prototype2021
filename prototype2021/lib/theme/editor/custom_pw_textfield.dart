9da78808b01f79a7d88bbb22d65a0a5093710489
Mina Kang <kasong13@unist.ac.kr>
2021-07-31 18:27:34 +0900

    Create custom_pw_textfield.dart
    - PW 블라인드 처리 기능 추가한 위젯 분리
    - 아이콘을 눌렀을 때 패스워드가 보이도록 구현해야 함

---
+++ prototype2021/lib/theme/editor/custom_pw_textfield.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class CustomPWTextField extends StatefulWidget {
+  String hintText;
+  Function(String) onChanged;
+  String initialText;
+  int maxLine;
+  CustomPWTextField(
+      {required this.hintText,
+      required this.onChanged,
+      this.initialText = '',
+      this.maxLine = 1});
+
+  @override
+  _CustomPWTextFieldState createState() => _CustomPWTextFieldState();
+}
+
+class _CustomPWTextFieldState extends State<CustomPWTextField> {
+  TextEditingController? _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = new TextEditingController(text: this.widget.initialText);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          height: 75,
+          decoration: BoxDecoration(
+            color: const Color(0xfff2f2f2),
+          ),
+          child: TextField(
+            controller: _controller,
+            decoration: new InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 30, right: 15),
+                hintText: this.widget.hintText),
+            onChanged: this.widget.onChanged,
+            maxLines: this.widget.maxLine,
+            obscureText: true,
+            keyboardType: TextInputType.multiline,
+            enableInteractiveSelection: false,
+          ),
+        ),
+        Positioned(
+          child: IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              'assets/icons/ic_eye_gray.png',
+            ),
+          ),
+          right: 10,
+          top: 13,
+        )
+      ],
+    );
+  }
+}
