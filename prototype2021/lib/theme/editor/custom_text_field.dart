f81b0794f28fad6cf5857ffdcc8059da6905cf8d
Mina Kang <kasong13@unist.ac.kr>
2021-08-03 15:50:32 +0900

    Update custom_text_field.dart
    - 기존 위젯에 커스텀 적용
    - 필드 명 변경
    - 인자 설정

--- prototype2021/lib/theme/editor/custom_text_field.dart
+++ prototype2021/lib/theme/editor/custom_text_field.dart
@@ -5,11 +5,16 @@ class CustomTextField extends StatefulWidget {
   Function(String) onChanged;
   String initialText;
   int maxLine;
+  bool isObscure = true;
+  bool isChecked2 = false;
+  bool isPasswordField;
+
   CustomTextField(
       {required this.hintText,
       required this.onChanged,
       this.initialText = '',
-      this.maxLine = 1});
+      this.maxLine = 1,
+      this.isPasswordField = false});
 
   @override
   _CustomTextFieldState createState() => _CustomTextFieldState();
@@ -26,20 +31,58 @@ class _CustomTextFieldState extends State<CustomTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Stack(
       children: [
-        TextField(
-          controller: _controller,
-          decoration: new InputDecoration(
-              border: InputBorder.none,
-              focusedBorder: InputBorder.none,
-              contentPadding:
-                  EdgeInsets.only(left: 15, bottom: 11, top: 30, right: 15),
-              hintText: this.widget.hintText),
-          onChanged: this.widget.onChanged,
-          maxLines: this.widget.maxLine,
-          keyboardType: TextInputType.multiline,
-          enableInteractiveSelection: false,
+        Container(
+          height: 75,
+          decoration: BoxDecoration(
+            color: const Color(0xfff2f2f2),
+          ),
+          child: TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                contentPadding:
+                    EdgeInsets.only(left: 15, bottom: 11, top: 30, right: 15),
+                hintText: this.widget.hintText),
+            onChanged: this.widget.onChanged,
+            maxLines: this.widget.maxLine,
+            obscureText:
+                this.widget.isPasswordField ? this.widget.isObscure : false,
+            keyboardType: TextInputType.multiline,
+            enableInteractiveSelection: false,
+          ),
+        ),
+        Container(
+          height: 75,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              this.widget.isPasswordField
+                  ? IconButton(
+                      icon: Image.asset(
+                        'assets/icons/ic_eye_gray.png',
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          this.widget.isObscure = !this.widget.isObscure;
+                        });
+                      })
+                  : SizedBox(),
+              IconButton(
+                  icon: Image.asset(
+                    'assets/icons/ic_search_remove_background.png',
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      this.widget.isChecked2 = !this.widget.isChecked2;
+                      _controller!.clear();
+                    });
+                  }),
+            ],
+          ),
         )
       ],
     );
