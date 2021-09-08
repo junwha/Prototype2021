6746d2600dcd32313752ef65e1539c5ad2a37b75
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-09-08 23:40:21 +0900

    Refactor: refactor signin_view_1.dart

--- prototype2021/lib/ui/signin_page/signin_view_1.dart
+++ prototype2021/lib/ui/signin_page/signin_view_1.dart
@@ -9,13 +9,73 @@ class SigninView1 extends StatefulWidget {
   _SigninView1State createState() => _SigninView1State();
 }
 
-class _SigninView1State extends State<SigninView1> {
-  List<bool> isChecked = [false, true];
-  bool isChecked1 = false;
-  bool isChecked2 = false;
-  bool isChecked3 = false;
+enum VerificationMethod { Phone, Email }
 
-  void _changeisChecked1(bool value) => setState(() => isChecked1 = value);
+class _SigninView1State extends State<SigninView1> {
+  VerificationMethod verificationMethod = VerificationMethod.Phone;
+  bool firstTermChecked = false;
+  bool secondTermChecked = false;
+  bool thirdTermChecked = false;
+
+  void setVerificationMethod(VerificationMethod _verificationMethod) {
+    setState(() {
+      verificationMethod = _verificationMethod;
+    });
+  }
+
+  void Function(bool?)? setTermCheckedFactory(int number) {
+    switch (number) {
+      case 1:
+        return (bool? isChecked) => setState(() {
+              firstTermChecked = isChecked ?? false;
+            });
+      case 2:
+        return (bool? isChecked) => setState(() {
+              secondTermChecked = isChecked ?? false;
+            });
+      case 3:
+        return (bool? isChecked) => setState(() {
+              thirdTermChecked = isChecked ?? false;
+            });
+      default:
+        break;
+    }
+  }
+
+  Row buildTermCheckbox(
+      {required bool isChecked,
+      required String text,
+      required void Function(bool?)? onCheckboxTap,
+      required void Function()? onDetailTap}) {
+    return Row(
+      children: [
+        Checkbox(
+          value: isChecked,
+          onChanged: onCheckboxTap,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            color: Color(0xff444444),
+            fontFamily: 'Roboto',
+          ),
+        ),
+        SizedBox(
+          width: 93,
+        ),
+        TextButton(
+            onPressed: onDetailTap,
+            child: Text("전체보기",
+                style: TextStyle(
+                  color: Color(0xff555555),
+                  fontSize: 12,
+                  fontFamily: 'Roboto',
+                  decoration: TextDecoration.underline,
+                )))
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,200 +89,105 @@ class _SigninView1State extends State<SigninView1> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                OutlinedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0.0))),
-                      side: MaterialStateProperty.resolveWith<BorderSide>(
-                          (Set<MaterialState> states) {
-                        final Color color =
-                            isChecked[1] ? Color(0xff4080ff) : Colors.grey;
-                        return BorderSide(color: color, width: 2);
-                      })),
-                  onPressed: () {
-                    setState(() {
-                      isChecked[0] = false;
-                      isChecked[1] = true;
-                    });
-                  },
-                  child: Container(
-                    height: 55,
-                    margin: EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "휴대폰으로 회원가입",
-                        style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 11 * pt,
-                          color: isChecked[1] ? Color(0xff4080ff) : Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
+                buildMethodTabButton(
+                  onPressed: () =>
+                      setVerificationMethod(VerificationMethod.Phone),
+                  isChecked: verificationMethod == VerificationMethod.Phone,
+                  text: "휴대폰으로 회원가입",
                 ),
-                OutlinedButton(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0.0))),
-                      side: MaterialStateProperty.resolveWith<BorderSide>(
-                          (Set<MaterialState> states) {
-                        final Color color =
-                            isChecked[0] ? Color(0xff4080ff) : Colors.grey;
-                        return BorderSide(color: color, width: 2);
-                      })),
-                  onPressed: () {
-                    setState(() {
-                      isChecked[0] = true;
-                      isChecked[1] = false;
-                    });
-                  },
-                  child: Container(
-                    height: 55,
-                    margin: EdgeInsets.only(
-                      left: 20,
-                      right: 20,
-                    ),
-                    child: Center(
-                      child: Text(
-                        "이메일로 회원가입",
-                        style: TextStyle(
-                          fontFamily: 'Roboto',
-                          fontSize: 11 * pt,
-                          color: isChecked[0] ? Color(0xff4080ff) : Colors.grey,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+                buildMethodTabButton(
+                    onPressed: () =>
+                        setVerificationMethod(VerificationMethod.Email),
+                    isChecked: verificationMethod == VerificationMethod.Email,
+                    text: "이메일로 회원가입")
               ],
             ),
             SizedBox(
               height: 75,
             ),
-            Row(
-              children: [
-                Checkbox(
-                  value: isChecked1,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked1 = value!;
-                    });
-                  },
-                ),
-                Text(
-                  '이용약관 동의 (필수)',
-                  style: TextStyle(
-                    color: Color(0xff444444),
-                    fontFamily: 'Roboto',
-                  ),
-                ),
-                SizedBox(
-                  width: 140,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("전체보기",
-                        style: TextStyle(
-                          color: Color(0xff555555),
-                          fontSize: 12,
-                          fontFamily: 'Roboto',
-                          decoration: TextDecoration.underline,
-                        )))
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: isChecked2,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked2 = value!;
-                    });
-                  },
-                ),
-                Text(
-                  '개인정보취급방침 동의 (필수)',
-                  style: TextStyle(
-                    color: Color(0xff444444),
-                    fontFamily: 'Roboto',
-                  ),
-                ),
-                SizedBox(
-                  width: 93,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("전체보기",
-                        style: TextStyle(
-                          color: Color(0xff555555),
-                          fontSize: 12,
-                          fontFamily: 'Roboto',
-                          decoration: TextDecoration.underline,
-                        )))
-              ],
-            ),
-            Row(
-              children: [
-                Checkbox(
-                  value: isChecked3,
-                  onChanged: (bool? value) {
-                    setState(() {
-                      isChecked3 = value!;
-                    });
-                  },
-                ),
-                Text(
-                  '마케팅 수신 동의 (선택)',
-                  style: TextStyle(
-                    color: Color(0xff444444),
-                    fontFamily: 'Roboto',
-                  ),
-                ),
-                SizedBox(
-                  width: 125,
-                ),
-                TextButton(
-                    onPressed: () {},
-                    child: Text("전체보기",
-                        style: TextStyle(
-                          color: Color(0xff555555),
-                          fontSize: 12,
-                          fontFamily: 'Roboto',
-                          decoration: TextDecoration.underline,
-                        ))),
-              ],
-            ),
+            buildTermCheckbox(
+                isChecked: firstTermChecked,
+                text: "이용약관 동의 (필수)",
+                onCheckboxTap: setTermCheckedFactory(1),
+                onDetailTap: () {}),
+            buildTermCheckbox(
+                isChecked: secondTermChecked,
+                text: "개인정보취급방침 동의 (필수)",
+                onCheckboxTap: setTermCheckedFactory(2),
+                onDetailTap: () {}),
+            buildTermCheckbox(
+                isChecked: thirdTermChecked,
+                text: "마케팅 수신 동의 (선택)",
+                onCheckboxTap: setTermCheckedFactory(3),
+                onDetailTap: () {}),
             SizedBox(
               height: 100,
             ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SigninView2()),
-                  );
-                },
-                child: Container(
-                    child: Center(
-                      child: Text(
-                        "휴대폰 인증",
-                        style: const TextStyle(
-                            color: const Color(0xffffffff),
-                            fontWeight: FontWeight.w700,
-                            fontFamily: "Roboto",
-                            fontStyle: FontStyle.normal,
-                            fontSize: 20.0),
-                      ),
-                    ),
-                    width: 400,
-                    height: 67,
-                    decoration: BoxDecoration(
-                      color: const Color(0xff4080ff),
-                    ))),
+            buildNextButton(),
           ],
+        ),
+      ),
+    );
+  }
+
+  TextButton buildNextButton() {
+    String methodToString =
+        verificationMethod == VerificationMethod.Email ? "이메일" : "휴대폰";
+    return TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SigninView2()),
+          );
+        },
+        child: Container(
+            child: Center(
+              child: Text(
+                "$methodToString 인증",
+                style: const TextStyle(
+                    color: const Color(0xffffffff),
+                    fontWeight: FontWeight.w700,
+                    fontFamily: "Roboto",
+                    fontStyle: FontStyle.normal,
+                    fontSize: 20.0),
+              ),
+            ),
+            width: 400,
+            height: 67,
+            decoration: BoxDecoration(
+              color: const Color(0xff4080ff),
+            )));
+  }
+
+  OutlinedButton buildMethodTabButton(
+      {required void Function() onPressed,
+      required bool isChecked,
+      required String text}) {
+    return OutlinedButton(
+      style: ButtonStyle(
+          shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0))),
+          side: MaterialStateProperty.resolveWith<BorderSide>(
+              (Set<MaterialState> states) {
+            final Color color = isChecked ? Color(0xff4080ff) : Colors.grey;
+            return BorderSide(color: color, width: 2);
+          })),
+      onPressed: onPressed,
+      child: Container(
+        height: 55,
+        margin: EdgeInsets.only(
+          left: 20,
+          right: 20,
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontFamily: 'Roboto',
+              fontSize: 11 * pt,
+              color: isChecked ? Color(0xff4080ff) : Colors.grey,
+            ),
+          ),
         ),
       ),
     );
