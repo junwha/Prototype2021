25fb6e236a3432dc3f404a2dfc00139a1579d7c5
준화 <junwha0511@naver.com>
2021-09-18 22:42:57 +0900

    set PlanMapModel as final

--- prototype2021/lib/theme/calendar/plan_map.dart
+++ prototype2021/lib/theme/calendar/plan_map.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:prototype2021/model/map/plan_map_model.dart';
+import 'package:prototype2021/theme/calendar/plan_list_item.dart';
 import 'package:prototype2021/theme/map/background_map.dart';
 import 'package:provider/provider.dart';
 
 class PlanMap extends StatefulWidget {
-  PlanMapModel model;
+  final PlanMapModel model;
 
   PlanMap({required this.model, Key? key}) : super(key: key);
 
