f2b976b1732147c798d8c60644d0d1633f68a2a5
준화 <junwha0511@naver.com>
2021-09-18 22:41:46 +0900

    Implement polyline

--- prototype2021/lib/model/map/plan_map_model.dart
+++ prototype2021/lib/model/map/plan_map_model.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:prototype2021/data/location.dart';
+import 'package:prototype2021/data/place_data_props.dart';
 import 'package:prototype2021/loader/google_place_loader.dart';
 import 'package:prototype2021/model/map/tb_map_model.dart';
 
 class PlanMapModel extends TBMapModel {
+  List<PlaceDataProps> placeItems;
   List<LatLng> get _polylinePoints =>
       this.locations.map((l) => l.latLng).toList();
 
   Polyline? get polyline => Polyline(
         polylineId: PolylineId("0"),
         points: _polylinePoints,
-        color: Colors.cyan,
-        width: 8,
+        color: Colors.black,
+        width: 3,
         patterns: [PatternItem.dash(10), PatternItem.gap(10)],
       );
 
-  PlanMapModel(LatLng center) : super(center) {
-    initPlan();
+  PlanMapModel(LatLng center, this.placeItems) : super(center) {
+    testPolyline();
   }
 
-  void initPlan() async {
+  void testPolyline() async {
     await this.markerList.loadImage();
-    this.addLocations([
-      Location(LatLng(35.5763, 129.1593), PlaceType.CAFFEE, "1"),
-      Location(LatLng(35.5864, 129.1892), PlaceType.CAFFEE, "2"),
-      Location(LatLng(35.5960, 129.1591), PlaceType.CAFFEE, "3"),
-      Location(LatLng(35.5863, 129.1890), PlaceType.CAFFEE, "4"),
-    ]);
+    this.updateLocations(placeItems.map((e) => Location(
+        LatLng(e.location.latitude, e.location.longitude),
+        PlaceType.CAFFEE,
+        e.name)));
   }
 }
