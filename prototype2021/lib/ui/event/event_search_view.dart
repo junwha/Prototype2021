d345e07f0caa3d0ef59ee5a7f4bf5efa3ddf75ac
Mina Kang <kasong13@unist.ac.kr>
2021-09-22 16:06:37 +0900

    Update event_search_view.dart

--- prototype2021/lib/ui/event/event_search_view.dart
+++ prototype2021/lib/ui/event/event_search_view.dart
@@ -101,92 +101,95 @@ class _EventSearchViewState extends State<EventSearchView> {
     final controller = FloatingSearchBarController();
     final _applyKey = GlobalKey<FormState>();
 
-    return FloatingSearchBar(
-      margins: EdgeInsets.only(left: 50),
-      shadowColor: Colors.transparent,
-      backdropColor: Colors.transparent,
-      borderRadius: BorderRadius.circular(50),
-      height: 45,
-      backgroundColor: Color.fromRGBO(106, 126, 166, 1),
-      controller: controller,
-      title: Row(
-        children: [
-          Image.asset("assets/icons/search_white.png"),
-          SizedBox(
-            width: 7,
-          ),
-          Text(
-            '장소, 여행지, 카페, 음식점 검색',
-            style: TextStyle(
-              fontSize: 16,
-              color: Color(0xffc5cddd),
-              fontWeight: FontWeight.normal,
-              fontFamily: 'Roboto',
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+      child: FloatingSearchBar(
+        margins: EdgeInsets.only(left: 50),
+        shadowColor: Colors.transparent,
+        backdropColor: Colors.transparent,
+        borderRadius: BorderRadius.circular(50),
+        height: 45,
+        backgroundColor: Color.fromRGBO(106, 126, 166, 1),
+        controller: controller,
+        title: Row(
+          children: [
+            Image.asset("assets/icons/search_white.png"),
+            SizedBox(
+              width: 7,
             ),
+            Text(
+              '장소, 여행지, 카페, 음식점 검색',
+              style: TextStyle(
+                fontSize: 16,
+                color: Color(0xffc5cddd),
+                fontWeight: FontWeight.normal,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ],
+        ),
+        hint: 'search',
+        scrollPadding: const EdgeInsets.only(top: 16, bottom: 16),
+        transitionDuration: const Duration(milliseconds: 1000),
+        transitionCurve: Curves.easeInOut,
+        physics: const BouncingScrollPhysics(),
+        axisAlignment: isPortrait ? 0.0 : -1.0,
+        openAxisAlignment: 0.0,
+        width: isPortrait ? 600 : 500,
+        debounceDelay: const Duration(milliseconds: 500),
+        automaticallyImplyBackButton: false,
+        onSubmitted: (query) {
+          // Call your model, bloc, controller here.
+          searchArticleModel.searchArticles(query);
+          setState(() {
+            this.previewData = searchArticleModel.eventArticleList;
+          });
+        },
+        onFocusChanged: (bool focus) {},
+        // Specify a custom transition to be used for
+        // animating between opened and closed stated.
+        transition: CircularFloatingSearchBarTransition(),
+        actions: [
+          FloatingSearchBarAction(
+            showIfOpened: false,
+            child: CircularButton(
+              icon: const Icon(Icons.filter_list),
+              onPressed: () {
+                //  Navigator.pushNamed(context, '/board/filter');
+              },
+            ),
+          ),
+          FloatingSearchBarAction.searchToClear(
+            showIfClosed: false,
           ),
         ],
-      ),
-      hint: 'search',
-      scrollPadding: const EdgeInsets.only(top: 16, bottom: 16),
-      transitionDuration: const Duration(milliseconds: 1000),
-      transitionCurve: Curves.easeInOut,
-      physics: const BouncingScrollPhysics(),
-      axisAlignment: isPortrait ? 0.0 : -1.0,
-      openAxisAlignment: 0.0,
-      width: isPortrait ? 600 : 500,
-      debounceDelay: const Duration(milliseconds: 500),
-      automaticallyImplyBackButton: false,
-      onSubmitted: (query) {
-        // Call your model, bloc, controller here.
-        searchArticleModel.searchArticles(query);
-        setState(() {
-          this.previewData = searchArticleModel.eventArticleList;
-        });
-      },
-      onFocusChanged: (bool focus) {},
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
-      transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: CircularButton(
-            icon: const Icon(Icons.filter_list),
-            onPressed: () {
-              //  Navigator.pushNamed(context, '/board/filter');
-            },
-          ),
-        ),
-        FloatingSearchBarAction.searchToClear(
-          showIfClosed: false,
-        ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                // Container(
-                //   height: 112,
-                //   width: MediaQuery.of(context).size.width,
-                //   color: Colors.white,
-                //   child: Center(
-                //     child: Text(
-                //       '서비스를 준비중입니다',
-                //       style: TextStyle(
-                //           color: Colors.black, fontWeight: FontWeight.bold),
-                //     ),
-                //   ),
-                // ),
-              ],
+        builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Material(
+              color: Colors.white,
+              elevation: 4.0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  // Container(
+                  //   height: 112,
+                  //   width: MediaQuery.of(context).size.width,
+                  //   color: Colors.white,
+                  //   child: Center(
+                  //     child: Text(
+                  //       '서비스를 준비중입니다',
+                  //       style: TextStyle(
+                  //           color: Colors.black, fontWeight: FontWeight.bold),
+                  //     ),
+                  //   ),
+                  // ),
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
