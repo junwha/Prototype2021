e849aad3cb194644b0eca5b2a2093329df04f07b
준화 <junwha0511@naver.com>
2021-09-30 21:23:47 +0900

    refactor: add padding to selectable text button

--- prototype2021/lib/theme/selectable_text_button.dart
+++ prototype2021/lib/theme/selectable_text_button.dart
@@ -5,9 +5,13 @@ class SelectableTextButton extends StatefulWidget {
   String titleName;
   bool isChecked;
   Function()? onPressed;
+  EdgeInsets padding;
 
   SelectableTextButton(
-      {required this.titleName, required this.isChecked, this.onPressed});
+      {required this.titleName,
+      required this.isChecked,
+      this.onPressed,
+      this.padding = const EdgeInsets.all(0)});
 
   @override
   _SelectableTextButtonState createState() => _SelectableTextButtonState();
@@ -16,25 +20,29 @@ class SelectableTextButton extends StatefulWidget {
 class _SelectableTextButtonState extends State<SelectableTextButton> {
   @override
   Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: ButtonStyle(
-          shape: MaterialStateProperty.all(RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0))),
-          side: MaterialStateProperty.resolveWith<BorderSide>(
-              (Set<MaterialState> states) {
-            return BorderSide(
-                color: this.widget.isChecked
-                    ? Color(0xffbbd2ff)
-                    : Color(0xffdbdbdb),
-                width: 2);
-          })),
-      onPressed: this.widget.onPressed,
-      child: Text(
-        this.widget.titleName,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 12 * pt,
-          color: this.widget.isChecked ? Color(0xff4080ff) : Color(0xffdbdbdb),
+    return Padding(
+      padding: this.widget.padding,
+      child: OutlinedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0))),
+            side: MaterialStateProperty.resolveWith<BorderSide>(
+                (Set<MaterialState> states) {
+              return BorderSide(
+                  color: this.widget.isChecked
+                      ? Color(0xffbbd2ff)
+                      : Color(0xffdbdbdb),
+                  width: 2);
+            })),
+        onPressed: this.widget.onPressed,
+        child: Text(
+          this.widget.titleName,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 12 * pt,
+            color:
+                this.widget.isChecked ? Color(0xff4080ff) : Color(0xffdbdbdb),
+          ),
         ),
       ),
     );
