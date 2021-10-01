dee399ffc9f2ce43ffa3c187cb21be46c505fae0
준화 <junwha0511@naver.com>
2021-10-02 04:42:25 +0900

    Implement SafeDELETE and HTTP log

--- prototype2021/lib/loader/safe_http.dart
+++ prototype2021/lib/loader/safe_http.dart
@@ -6,6 +6,13 @@ import 'package:http/http.dart' show Response;
 import 'package:prototype2021/model/safe_http_dto/base.dart';
 import 'package:prototype2021/settings/constants.dart';
 
+void printHTTPLog(http.Response res) {
+  if (SAFE_HTTP_DEBUG) {
+    print(res.body);
+    print(res.statusCode);
+  }
+}
+
 /* 
  * 함수를 부를때 타입 I, O는 필수로 넣어주셔야 합니다.
  * 타입이 주어지지 않으면 return 값이 제대로 나오지 않습니다
@@ -19,7 +26,7 @@ Future<SafeMutationOutput<O>>
   try {
     Response res = await http.post(dto.getUrlWithParams(),
         headers: dto.getHeaders(), body: dto.getJsonString());
-    if (SAFE_HTTP_DEBUG) print(res.body);
+    printHTTPLog(res);
     if (res.statusCode == expectedCode) {
       String data = fromBytes ? utf8.decode(res.bodyBytes) : res.body;
       return new SafeMutationOutput<O>(success: true, data: data);
@@ -44,7 +51,7 @@ Future<SafeMutationOutput<O>>
   try {
     Response res = await http.put(dto.getUrlWithParams(),
         headers: dto.getHeaders(), body: dto.getJsonString());
-    if (SAFE_HTTP_DEBUG) print(res.body);
+    printHTTPLog(res);
     if (res.statusCode == expectedCode) {
       String data = fromBytes ? utf8.decode(res.bodyBytes) : res.body;
       return new SafeMutationOutput<O>(success: true, data: data);
@@ -69,7 +76,7 @@ Future<SafeMutationOutput<O>>
   try {
     Response res = await http.patch(dto.getUrlWithParams(),
         headers: dto.getHeaders(), body: dto.getJsonString());
-    if (SAFE_HTTP_DEBUG) print(res.body);
+    printHTTPLog(res);
     if (res.statusCode == expectedCode) {
       String data = fromBytes ? utf8.decode(res.bodyBytes) : res.body;
       return new SafeMutationOutput<O>(success: true, data: data);
@@ -94,7 +101,28 @@ Future<SafeQueryOutput<O>>
   try {
     Response res =
         await http.get(dto.getUrlWithParams(), headers: dto.getHeaders());
-    if (SAFE_HTTP_DEBUG) print(res.body);
+    printHTTPLog(res);
+    if (res.statusCode == expectedCode) {
+      String data = fromBytes ? utf8.decode(res.bodyBytes) : res.body;
+      return new SafeQueryOutput<O>(success: true, data: data);
+    }
+    throw new HttpException("[${res.statusCode} : ${res.body.toString()}]");
+  } catch (error) {
+    return new SafeQueryOutput<O>(
+        success: false, error: new SafeHttpError(message: error.toString()));
+  }
+}
+
+Future<SafeQueryOutput<O>>
+    safeDELETE<I extends SafeHttpDataInput, O extends SafeHttpDataOutput>(
+  SafeQueryInput<I> dto, [
+  int expectedCode = 204,
+  bool fromBytes = false,
+]) async {
+  try {
+    Response res =
+        await http.delete(dto.getUrlWithParams(), headers: dto.getHeaders());
+    printHTTPLog(res);
     if (res.statusCode == expectedCode) {
       String data = fromBytes ? utf8.decode(res.bodyBytes) : res.body;
       return new SafeQueryOutput<O>(success: true, data: data);
