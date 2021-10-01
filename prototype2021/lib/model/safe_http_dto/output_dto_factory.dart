39d0d467e654eda630a0e5e848f287f7fd8983bc
준화 <junwha0511@naver.com>
2021-10-02 02:40:28 +0900

    throw exception if T is not defined in the factory

--- prototype2021/lib/model/safe_http_dto/output_dto_factory.dart
+++ prototype2021/lib/model/safe_http_dto/output_dto_factory.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:prototype2021/model/safe_http_dto/base.dart';
 import 'package:prototype2021/model/safe_http_dto/get/contents.dart';
 import 'package:prototype2021/model/safe_http_dto/patch/heart.dart';
@@ -19,11 +20,20 @@ final _factories = <Type, SafeHttpDataOutput Function(Map<String, dynamic>)>{
   ContentsListOutput: (json) => ContentsListOutput.fromJson(json: json),
   ContentsWishlistOutput: (json) => ContentsWishlistOutput.fromJson(json: json),
   ContentsDetailOutput: (json) => ContentsDetailOutput.fromJson(json: json),
+
   // This key-value pair is for test purpose
   ExampleOutput: (json) => ExampleOutput.fromJson(json: json),
 };
 
+class FactoryNotDefined<T> implements Exception {
+  final dynamic message = T.toString() +
+      " isn't defined in the output dto factory (output_dto_factory.dart)";
+
+  FactoryNotDefined();
+}
+
 T generateOutput<T extends SafeHttpDataOutput>(String jsonString) {
+  if (!_factories.containsKey(T)) throw FactoryNotDefined<T>();
   return _factories[T]!(jsonDecode(jsonString)) as T;
 }
 
