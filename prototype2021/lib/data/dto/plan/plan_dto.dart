eb50ea0dd870e78c1bf0205f58100ea6ba68a69e
준화 <junwha0511@naver.com>
2021-10-02 03:39:38 +0900

    extract PlanProps from PlanPreview DTO

--- prototype2021/lib/data/dto/plan/plan_dto.dart
+++ prototype2021/lib/data/dto/plan/plan_dto.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:prototype2021/data/location_data.dart';
 
-class PlanPreview {
+class PlanProps {
   final int id;
   final String title;
   final String area;
@@ -9,9 +9,8 @@ class PlanPreview {
   final List<String> types;
   final String expense;
   final DateTimeRange period;
-  final bool hearted;
 
-  PlanPreview.fromJson({required Map<String, dynamic> json})
+  PlanProps.fromJson({required Map<String, dynamic> json})
       : id = json["id"],
         title = json["title"],
         area = (json["area_code"] as List<dynamic>)
@@ -24,8 +23,14 @@ class PlanPreview {
         expense = expenseCodeToString[json["expense"]] ?? "",
         period = DateTimeRange(
             start: DateTime.parse(json["start_date"]),
-            end: DateTime.parse(json["end_date"])),
-        hearted = json["hearted"];
+            end: DateTime.parse(json["end_date"]));
+}
+
+class PlanPreview extends PlanProps {
+  final bool hearted;
+  PlanPreview.fromJson({required Map<String, dynamic> json})
+      : hearted = json["hearted"],
+        super.fromJson(json: json);
 }
 
 Map<int, String> expenseCodeToString = {
