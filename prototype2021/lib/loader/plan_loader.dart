2991947c349eb6f34fe56199446a181707214ea3
준화 <junwha0511@naver.com>
2021-10-02 00:33:53 +0900

    Implement plan list loader with pagination

--- prototype2021/lib/loader/plan_loader.dart
+++ prototype2021/lib/loader/plan_loader.dart
@@ -1,14 +1,25 @@
 import 'dart:io';
 
+import 'package:flutter/services.dart';
+import 'package:prototype2021/data/dto/plan/plan_preview.dart';
 import 'package:prototype2021/loader/safe_http.dart';
 import 'package:prototype2021/model/safe_http_dto/base.dart';
+import 'package:prototype2021/model/safe_http_dto/common.dart';
+import 'package:prototype2021/model/safe_http_dto/get/plan.dart';
 import 'package:prototype2021/model/safe_http_dto/get/verification.dart';
 import 'package:prototype2021/model/safe_http_dto/patch/heart.dart';
 import 'package:prototype2021/settings/constants.dart';
 
-class PlanLoader {
-  // Custom Functions
+enum PaginationState {
+  start,
+  mid,
+  end,
+}
 
+class PlanLoader {
+  PaginationState pagination = PaginationState.start;
+
+  // Custom Functions
   Future<void> heartPlan(String planId, String token) async {
     PlanHeartInput params = new PlanHeartInput(planId: planId);
     SafeMutationInput<PlanHeartInput> dto =
@@ -19,13 +30,41 @@ class PlanLoader {
       throw HttpException(result.error?.message ?? "Unexpected error");
   }
 
+  Future<List<PlanPreview>> getPlanList(String token) async {
+    if (pagination == PaginationState.end) return [];
+
+    PlanListInput params = PlanListInput();
+
+    SafeQueryInput<PlanListInput> dto = SafeQueryInput<PlanListInput>(
+        params: params, url: planListUrl, token: token);
+    SafeQueryOutput<PlanListOutput> result = await planList(dto);
+
+    if (result.success) {
+      if (result.data!.next != null) {
+        planListUrl = result.data!.next!;
+        pagination = PaginationState.mid;
+      } else {
+        planListUrl = "";
+        pagination = PaginationState.end;
+      }
+      return result.data!.results;
+    }
+
+    print(result.error!);
+    return [];
+  }
+
   // Fetching Functions
 
   Future<SafeMutationOutput<PlanHeartOutput>> planHeart(
           SafeMutationInput<PlanHeartInput> dto) async =>
       await safePatch<PlanHeartInput, PlanHeartOutput>(dto);
 
+  Future<SafeQueryOutput<PlanListOutput>> planList(
+          SafeQueryInput<PlanListInput> dto) async =>
+      await safeGET<PlanListInput, PlanListOutput>(dto);
   // Endpoints
 
   String planHeartUrl = "$apiBaseUrl/plan/:planId/like";
+  String planListUrl = "$apiBaseUrl/plan";
 }
--- prototype2021/lib/model/safe_http_dto/common.dart
+++ prototype2021/lib/model/safe_http_dto/common.dart
@@ -2,20 +2,20 @@ import 'package:prototype2021/model/safe_http_dto/base.dart';
 
 abstract class Pagination<T> {
   abstract final int count;
-  abstract final int? next;
-  abstract final int? previous;
+  abstract final String? next;
+  abstract final String? previous;
   abstract final List<T> results;
 }
 
 class PaginationOutput extends SafeHttpDataOutput {
   final int count;
-  final int? next;
-  final int? previous;
+  final String? next;
+  final String? previous;
 
   PaginationOutput.fromJson({required Map<String, dynamic> json})
       : count = json["count"] as int,
-        next = nullable<int>(json["next"]),
-        previous = nullable<int>(json["previous"]);
+        next = nullable<String>(json["next"]),
+        previous = nullable<String>(json["previous"]);
 }
 
 T? nullable<T>(T? value) => value == null ? null : value as T;
