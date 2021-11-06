b5e91ead708f969ca17c6de1ef7c428dac2d609f
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-11-06 15:16:27 +0900

    Add logout

--- lib/views/mypage/my_page_view.dart
+++ lib/views/mypage/my_page_view.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:prototype2021/handler/user/user_info_handler.dart';
+import 'package:prototype2021/utils/simple_storage/simple_storage.dart';
+import 'package:prototype2021/views/login/login_view.dart';
 import 'package:prototype2021/widgets/cards/flat_card.dart';
 import 'package:prototype2021/widgets/shapes/circular_image.dart';
+import 'package:provider/provider.dart';
 
 class MyPage extends StatelessWidget {
   @override
@@ -31,27 +35,45 @@ class MyPage extends StatelessWidget {
               return SingleChildScrollView(
                   child: Column(
                 children: [
-                  buildProfile(),
+                  buildProfile(context),
                   ...buildCards(),
                 ],
               ));
             }));
   }
 
-  Container buildProfile() {
+  Container buildProfile(BuildContext context) {
+    UserInfoHandler infoHandler = Provider.of<UserInfoHandler>(context);
     return Container(
       child: Row(
         children: [
           CircularImage(),
           SizedBox(width: 10),
-          Text("Some text",
-              style: const TextStyle(
-                  color: const Color(0xff191919),
-                  fontWeight: FontWeight.w700,
-                  fontFamily: "Roboto",
-                  fontStyle: FontStyle.normal,
-                  fontSize: 22.0),
-              textAlign: TextAlign.center)
+          Column(
+            children: [
+              Text("프로필 이름",
+                  style: const TextStyle(
+                      color: const Color(0xff191919),
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Roboto",
+                      fontStyle: FontStyle.normal,
+                      fontSize: 22.0),
+                  textAlign: TextAlign.center),
+              TextButton(
+                onPressed: () {
+                  infoHandler.setToken(null);
+                  SimpleStorage.writeString(SimpleStorageKeys.jwtToken, "");
+                  Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (_) => LoginView()),
+                    (route) => false,
+                  );
+                },
+                child: Text("로그아웃"),
+              ),
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+          ),
         ],
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
